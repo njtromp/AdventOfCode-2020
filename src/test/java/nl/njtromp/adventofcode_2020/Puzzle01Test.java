37c7479feb77dc2766bd517c22a812a61afc7b94
Nico Tromp <n.j.tromp@hva.nl>
2020-12-01 15:33:51 +0100

    Puzzle 2 solved.

--- src/test/java/nl/njtromp/adventofcode_2020/Puzzle01Test.java
+++ src/test/java/nl/njtromp/adventofcode_2020/Puzzle01Test.java
@@ -6,9 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Puzzle01Test {
     @Test
-    void checkExample() {
+    void checkExamplePart1() {
         Integer[] numbers = {1721, 979, 366, 299, 675, 1456};
-        assertEquals(514579, Puzzle01.solve(numbers));
+        assertEquals(514579, Puzzle01.solvePart1(numbers));
+    }
+
+    @Test
+    void checkExamplePart2() {
+        Integer[] numbers = {1721, 979, 366, 299, 675, 1456};
+        assertEquals(241861950, Puzzle01.solvePart2(numbers));
     }
 
 }
