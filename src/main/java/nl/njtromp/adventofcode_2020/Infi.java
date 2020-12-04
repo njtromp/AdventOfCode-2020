e99f4213502e77ca8e7cbffa4f9b13d13b012111
Nico Tromp <n.j.tromp@hva.nl>
2020-12-04 14:48:12 +0100

    Infi puzzle 2.

--- src/main/java/nl/njtromp/adventofcode_2020/Infi.java
+++ src/main/java/nl/njtromp/adventofcode_2020/Infi.java
@@ -1,23 +1,33 @@
 package nl.njtromp.adventofcode_2020;
 
+import java.util.Arrays;
+
 public class Infi {
-    static int aantalPakjes(int lengteZijkant) {
+    static long aantalPakjes(long lengteZijkant) {
         return
                 3 * lengteZijkant * lengteZijkant + // Middenstuk horizontaal
-                4 * sommatie(lengteZijkant - 1) + // Schuine zijden (4 halve driehoeken)
-                2 * (lengteZijkant * lengteZijkant); // Rechte stukken tussen de schuive zijden (onder en boven)
+                4 * sommatie(lengteZijkant) +       // Schuine zijden (4 halve driehoeken)
+                2 * lengteZijkant * lengteZijkant;  // Rechte stukken tussen de schuine zijden (onder en boven)
 
     }
 
-    private static int sommatie(int n) {
-        return n == 0 ? 0 : n + sommatie(n - 1);
+    private static long sommatie(long n) {
+        return (n-1)*n/2;
+    }
+
+    private static long bepaalLengte(long aantalInwoners) {
+        long lengte = 1;
+        while (aantalPakjes(lengte) < aantalInwoners) {
+            lengte++;
+        }
+        return lengte;
     }
 
     public static void main(String[] args) {
-        int lengte = 1;
-        while (aantalPakjes(lengte) < 17493412) {
-            lengte++;
-        }
+        long lengte = bepaalLengte(17_493_412);
         System.out.printf("De minimale lengte van een zijde is: %d\n", lengte);
+        long[] aantalInwoners = {42_732_096L, 369_030_498L, 430_839_868L, 747_685_826L, 1_340_952_816L, 4_541_536_619L};
+        System.out.printf("Totaal aantal lappen stof: %d\n", Arrays.stream(aantalInwoners).map(Infi::bepaalLengte).sum() * 8);
     }
+
 }
