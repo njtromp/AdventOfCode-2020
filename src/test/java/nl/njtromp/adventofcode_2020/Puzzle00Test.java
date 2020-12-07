a6c10baba88a88109d9bc57300d2db590a0b5586
Nico Tromp <nico.tromp@gmail.com>
2020-12-07 11:59:07 +0100

    Add skeleton for new puzzles.

---
+++ src/test/java/nl/njtromp/adventofcode_2020/Puzzle00Test.java
@@ -0,0 +1,23 @@
+package nl.njtromp.adventofcode_2020;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Puzzle00Test {
+
+    static String[] example = {};
+
+    @Test
+    void checkExamplePart1() {
+        assertEquals(0, Puzzle00.solvePart1(example));
+    }
+
+    @Disabled("Enable after part 1 has been solved!")
+    @Test
+    void checkExamplePart2() {
+        assertEquals(0, Puzzle00.solvePart2(example));
+    }
+
+}
