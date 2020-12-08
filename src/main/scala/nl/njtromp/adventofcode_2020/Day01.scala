658a62244a7de92687bc00cfbe8061fc86eee96f
Nico Tromp <nico.tromp@gmail.com>
2020-12-08 08:29:22 +0100

    Add spaces in math expression.

--- src/main/scala/nl/njtromp/adventofcode_2020/Day01.scala
+++ src/main/scala/nl/njtromp/adventofcode_2020/Day01.scala
@@ -9,7 +9,7 @@ object Day01 extends App {
   }
   val answerPart1 = numbers.filter(n => numbers.contains(2020 - n)).product
   println(s"Answer part 1: $answerPart1");
-  val answerPart2 = numbers.filter(n => numbers.filter(m => numbers.contains(2020-n-m)).size > 0).product
+  val answerPart2 = numbers.filter(n => numbers.filter(m => numbers.contains(2020 - n - m)).size > 0).product
   println(s"Answer part 2: $answerPart2");
 
 }
