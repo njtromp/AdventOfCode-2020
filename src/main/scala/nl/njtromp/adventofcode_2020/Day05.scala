9e51adcef93abc036483f78c6b4fb49f0a0c9275
Nico Tromp <nico.tromp@gmail.com>
2020-12-08 20:25:09 +0100

    Solved day 5.

---
+++ src/main/scala/nl/njtromp/adventofcode_2020/Day05.scala
@@ -0,0 +1,12 @@
+package nl.njtromp.adventofcode_2020
+
+import scala.io.Source
+
+object Day05 extends App {
+  private var seatNumbers: Set[Int] = Set.empty
+  for (line <- Source.fromInputStream(Day01.getClass.getResourceAsStream("/input-puzzle05.txt")).getLines) {
+    seatNumbers += Integer.parseInt(line.replaceAll("[FL]", "0").replaceAll("[BR]", "1"), 2)
+  }
+  println(s"Answer part 1: " + seatNumbers.max);
+  println(s"Answer part 2: " + (8 to 127 * 8).filter(n => !(seatNumbers contains n) && (seatNumbers contains (n - 1)) && (seatNumbers contains (n + 1))).head);
+}
