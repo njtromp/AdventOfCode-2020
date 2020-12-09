02736129169affac209e495016d3046799cea9ac
Nico Tromp <nico.tromp@gmail.com>
2020-12-09 06:56:17 +0100

    Puzzles day 9 solved (24/33).

--- src/main/java/nl/njtromp/adventofcode_2020/Puzzle09.java
+++ src/main/java/nl/njtromp/adventofcode_2020/Puzzle09.java
@@ -1,22 +1,58 @@
 package nl.njtromp.adventofcode_2020;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Puzzle09 {
     public static void main(String[] args) {
-        String[] input = readInput();
-        System.out.printf("Answer part 1: %d\n", solvePart1(input));
-//        System.out.printf("Answer part 2: %d\n", solvePart2(input));
+        List<Long> input = Arrays.stream(readInput()).map(Long::parseLong).collect(Collectors.toList());
+        System.out.printf("Answer part 1: %d\n", solvePart1(input, 25));
+        System.out.printf("Answer part 2: %d\n", solvePart2(input, 25));
     }
 
-    static int solvePart1(String[] lines) {
+    static long solvePart1(List<Long> input, int preamble) {
+        LinkedList<Long> numbers = new LinkedList<>();
+        for (long number : input) {
+            if (numbers.size() == preamble) {
+                if (!canBeConstructes(number, numbers)) {
+                    return number;
+                }
+            }
+            numbers.addLast(number);
+            if (numbers.size() > preamble) {
+                numbers.removeFirst();
+            }
+        }
         return -1;
     }
 
-    static int solvePart2(String[] lines) {
-        return -1;
+    static long solvePart2(List<Long> input, int preamble) {
+        final long toMatch = solvePart1(input, preamble);
+        int min = 0;
+        int max = min;
+        long remainder = toMatch;
+        while (remainder > 0) {
+            while (remainder > 0) {
+                remainder -= input.get(max);
+                max++;
+            }
+            if (remainder == 0) {
+                return input.subList(min, max).stream().min(Long::compareTo).get() + input.subList(min, max).stream().max(Long::compareTo).get();
+            }
+            remainder = toMatch;
+            min++;
+            max = min;
+        }
+        return -1L;
+    }
+
+    private static boolean canBeConstructes(long number, LinkedList<Long> numbers) {
+        for (long i : numbers) {
+            if (numbers.contains(number - i) && number != i) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static String[] readInput() {
--- src/test/java/nl/njtromp/adventofcode_2020/Puzzle09Test.java
+++ src/test/java/nl/njtromp/adventofcode_2020/Puzzle09Test.java
@@ -1,23 +1,44 @@
 package nl.njtromp.adventofcode_2020;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Puzzle09Test {
 
-    static String[] example = {};
+    List<Long> example = List.of(
+            35L,
+            20L,
+            15L,
+            25L,
+            47L,
+            40L,
+            62L,
+            55L,
+            65L,
+            95L,
+            102L,
+            117L,
+            150L,
+            182L,
+            127L,
+            219L,
+            299L,
+            277L,
+            309L,
+            576L
+    );
 
     @Test
     void checkExamplePart1() {
-        assertEquals(0, Puzzle00.solvePart1(example));
+        assertEquals(127, Puzzle09.solvePart1(example, 5));
     }
 
-    @Disabled("Enable after part 1 has been solved!")
     @Test
     void checkExamplePart2() {
-        assertEquals(0, Puzzle00.solvePart2(example));
+        assertEquals(62, Puzzle09.solvePart2(example, 5));
     }
 
 }
