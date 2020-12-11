040f65d4ca38dfc7174e621e756adfc4110bb1f5
Nico Tromp <nico.tromp@gmail.com>
2020-12-11 20:30:54 +0100

    Ready for puzzle 12.

---
+++ src/test/java/nl/njtromp/adventofcode_2020/Puzzle12Test.java
@@ -0,0 +1,23 @@
+package nl.njtromp.adventofcode_2020;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Puzzle12Test {
+
+    static String[] example = {};
+
+    @Test
+    void checkExamplePart1() {
+        assertEquals(0, Puzzle12.solvePart1(example));
+    }
+
+    @Disabled("Enable after part 1 has been solved!")
+    @Test
+    void checkExamplePart2() {
+        assertEquals(0, Puzzle12.solvePart2(example));
+    }
+
+}
