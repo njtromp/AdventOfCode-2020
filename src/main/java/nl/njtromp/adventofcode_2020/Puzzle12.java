64525dae1d0663fd10e782a458cc5a0f62c12059
Nico Tromp <nico.tromp@gmail.com>
2020-12-12 09:42:36 +0100

    Puzzle 12 part 1.

--- src/main/java/nl/njtromp/adventofcode_2020/Puzzle12.java
+++ src/main/java/nl/njtromp/adventofcode_2020/Puzzle12.java
@@ -12,7 +12,44 @@ public class Puzzle12 {
     }
 
     static int solvePart1(String[] lines) {
-        return -1;
+        int heading = 0;
+        int x = 0;
+        int y = 0;
+        for (String line : lines) {
+            if (line.charAt(0) == 'N') {
+                y += Integer.parseInt(line.substring(1));
+            } else if (line.charAt(0) == 'S') {
+                y -= Integer.parseInt(line.substring(1));
+            } else if (line.charAt(0) == 'E') {
+                x += Integer.parseInt(line.substring(1));
+            } else if (line.charAt(0) == 'W') {
+                x -= Integer.parseInt(line.substring(1));
+            } else if (line.charAt(0) == 'L') {
+                heading = Math.abs(360 + heading + Integer.parseInt(line.substring(1))) % 360;
+            } else if (line.charAt(0) == 'R') {
+                heading = Math.abs(360 + heading - Integer.parseInt(line.substring(1))) % 360;
+            } else if (line.charAt(0) == 'F') {
+                switch (heading) {
+                    case 0 :
+                        x += Integer.parseInt(line.substring(1));
+                        break;
+                    case 90 :
+                        y += Integer.parseInt(line.substring(1));
+                        break;
+                    case 180 :
+                        x -= Integer.parseInt(line.substring(1));
+                        break;
+                    case 270 :
+                        y -= Integer.parseInt(line.substring(1));
+                        break;
+                    default:
+                        System.out.printf("Damn it not a heading [%d] I can handle...\n", heading);
+                }
+            } else {
+                System.out.printf("Unknown instruction [%s]!\n", line);
+            }
+        }
+        return Math.abs(x) + Math.abs(y);
     }
 
     static int solvePart2(String[] lines) {
@@ -21,7 +58,7 @@ public class Puzzle12 {
 
     private static String[] readInput() {
         List<String> lines = new ArrayList<>();
-        Scanner inputFile = new Scanner(Puzzle12.class.getResourceAsStream("/input-puzzle12 .txt"));
+        Scanner inputFile = new Scanner(Puzzle12.class.getResourceAsStream("/input-puzzle12.txt"));
         while (inputFile.hasNextLine()) {
             lines.add(inputFile.nextLine());
         }
