e9da166dd57b87e7020b40b85b6cba23f4ebe662
Nico Tromp <nico.tromp@gmail.com>
2020-12-13 11:39:55 +0100

    Puzzle 13 part 2 is running.

--- src/main/scala/nl/njtromp/adventofcode_2020/Day13.scala
+++ src/main/scala/nl/njtromp/adventofcode_2020/Day13.scala
@@ -3,18 +3,23 @@ package nl.njtromp.adventofcode_2020
 import scala.io.Source
 
 object Day13 extends App {
-  val ns: Iterator[String] = Source.fromInputStream(Day01.getClass.getResourceAsStream("/input-puzzle13.txt")).getLines()
+  val lines: List[String] = Source.fromInputStream(Day01.getClass.getResourceAsStream("/input-puzzle13.txt")).getLines().toList
 
-  def solvePart1(ns: Iterator[String]):Int = {
-    val asList = ns.toList
-    val time = asList(0).toInt
-    val busses = asList(1).split(",").filter(id => id != "x").map(id => (id.toInt, id.toInt - time % id.toInt)).sortBy(i => i._2).toList
-    val bus = busses.head
+  def solvePart1(lines: List[String]):Int = {
+    val time = lines(0).toInt
+    val bus = lines(1).split(",").filter(id => id != "x").map(id => (id.toInt, id.toInt - time % id.toInt)).sortBy(i => i._2).toList.head
     bus._1 * bus._2
   }
 
-  def solvePart2(ns: Iterator[String]): Int = -1
+  def solvePart2(lines: List[String]): Long = {
+    val busses = lines(1).split(",").zipWithIndex.filter(b => b._1 != "x").map(b => (b._1.toInt, b._2))
+    var t: Long = 0
+    while (t >= 0 && !busses.tail.forall(b => b._1 - (t % b._1) == b._2)) {
+      t += busses.head._1
+    }
+    t
+  }
 
-  println(s"Answer part 1: ${solvePart1(ns)}")
-  println(s"Answer part 2: ${solvePart2(ns)}")
+  println(s"Answer part 1: ${solvePart1(lines)}")
+  println(s"Answer part 2: ${solvePart2(lines)}")
 }
--- src/test/scala/nl/njtromp/adventofcode_2020/Day13Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2020/Day13Test.scala
@@ -3,13 +3,39 @@ package nl.njtromp.adventofcode_2020
 import org.scalatest.flatspec.AnyFlatSpec
 
 class Day13Test extends AnyFlatSpec {
-  val example = Iterator("939", "7,13,x,x,59,x,31,19")
+  val example = List("939", "7,13,x,x,59,x,31,19")
 
-  "Part 1 " should " solve example correct " in {
+  "Part 1 " should " solve example correct" in {
     assert(Day13.solvePart1(example) == 295)
   }
 
-  "Part 2 " should " solve example correct " in {
+  "Part 2 " should " solve example correct" in {
     assert(Day13.solvePart2(example) == 1068781)
   }
+
+  "Part 2 " should "solve also extra example 1 correct" in {
+    val example = List("", "17,x,13,19")
+    assert(Day13.solvePart2(example) == 3417)
+  }
+
+  "Part 2 " should "solve also extra example 2 correct" in {
+    val example = List("", "67,7,59,61")
+    assert(Day13.solvePart2(example) == 754018)
+  }
+
+  "Part 2 " should "solve also extra example 3 correct" in {
+    val example = List("", "67,x,7,59,61")
+    assert(Day13.solvePart2(example) == 7792100)
+  }
+
+  "Part 2 " should "solve also extra example 4 correct" in {
+    val example = List("", "67,7,x,59,61")
+    assert(Day13.solvePart2(example) == 1261476)
+  }
+
+  "Part 2 " should "solve also extra example 5 correct" in {
+    val example = List("", "1789,37,47,1889")
+    assert(Day13.solvePart2(example) == 1202161486)
+  }
+
 }
