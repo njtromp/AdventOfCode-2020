c204ae8135344397a87bf906e2e574f2157fa426
Nico Tromp <nico.tromp@gmail.com>
2020-12-14 20:01:09 +0100

    Add classname to the output.

--- src/main/scala/nl/njtromp/adventofcode_2020/Puzzle.scala
+++ src/main/scala/nl/njtromp/adventofcode_2020/Puzzle.scala
@@ -6,8 +6,8 @@ trait Puzzle {
 
   def solvePuzzles(inputName: String): Unit = {
     val lines: List[String] = Source.fromInputStream(getClass.getResourceAsStream(inputName)).getLines().toList
-    println(s"Answer part 1: ${solvePart1(lines)}")
-    println(s"Answer part 2: ${solvePart2(lines)}")
+    println(s"Answer ${getClass.getSimpleName} part 1: ${solvePart1(lines)}")
+    println(s"Answer ${getClass.getSimpleName} part 2: ${solvePart2(lines)}")
   }
 
   def solvePart1(lines: List[String]): Long;
