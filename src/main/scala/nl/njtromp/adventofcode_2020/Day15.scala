e165e443ec570644928994c422bfbd259c202d67
Nico Tromp <nico.tromp@gmail.com>
2020-12-15 08:20:17 +0100

    Replace if-else with two cases.

--- src/main/scala/nl/njtromp/adventofcode_2020/Day15.scala
+++ src/main/scala/nl/njtromp/adventofcode_2020/Day15.scala
@@ -22,13 +22,12 @@ class Day15 extends Puzzle {
     while (turn < maxTurns) {
       turn += 1L
       spokenInTurn.get(lastSpoken) match {
-        case Some((t, p)) =>
-          if (p == 0) lastSpoken = 0L else lastSpoken = t - p
-        case None =>
-          lastSpoken = 0L
+        case Some((t, 0L)) => lastSpoken = 0L
+        case Some((t, p)) => lastSpoken = t - p
+        case None => lastSpoken = 0L
       }
       spokenInTurn.get(lastSpoken) match {
-        case Some((t, n)) => spokenInTurn += (lastSpoken -> (turn, t))
+        case Some((t, _)) => spokenInTurn += (lastSpoken -> (turn, t))
         case None => spokenInTurn += (lastSpoken -> (turn, 0L))
       }
     }
