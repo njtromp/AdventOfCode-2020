7151b1d25e6e7a8f36a50770e2b680043e23b3cb
Nico Tromp <nico.tromp@gmail.com>
2020-12-16 08:50:34 +0100

    Puzzle 16 part 1 solved (very ugly).

--- src/main/scala/nl/njtromp/adventofcode_2020/Day16.scala
+++ src/main/scala/nl/njtromp/adventofcode_2020/Day16.scala
@@ -1,12 +1,38 @@
 package nl.njtromp.adventofcode_2020
 
+import scala.collection.mutable
+
 class Day16 extends Puzzle {
-  private val rule = "(.+): (\\d+)-(\\d+) or (\\d+)-(\\d+)".r
-  private val yourTicket = "your ticket:".r
-  private val nearbyTickets = "nearby tickets:".r
+  private val Rule = "(.+): (\\d+)-(\\d+) or (\\d+)-(\\d+)".r
+  private val MyTicket = "your ticket:".r
+  private val NearbyTickets = "nearby tickets:".r
 
   def solvePart1(lines: List[String]): Long = {
-
+    var rules: mutable.HashMap[String, List[(Int, Int)]] = mutable.HashMap.empty
+    var myTicket: List[Int] = List.empty
+    var nearbyTickets: List[List[Int]] = List(List.empty)
+    var scanningState: Int = 0;
+    for (line <- lines) {
+      if (line.nonEmpty) {
+        if (scanningState == 0) {
+          line match {
+            case Rule(name, l1, u1, l2, u2) =>
+              rules += (name -> List((l1.toInt, u1.toInt), (l2.toInt, u2.toInt)))
+            case MyTicket() => scanningState = 1
+          }
+        } else if (scanningState == 1) {
+          line match {
+            case NearbyTickets() => scanningState = 2
+            case _ => myTicket = line.split(",").map(_.toInt).toList
+          }
+        } else {
+          nearbyTickets = line.split(",").map(_.toInt).toList +: nearbyTickets
+        }
+      }
+    }
+    val rs: List[(Int, Int)] = rules.values.toList.flatMap(rs => rs)
+    val ns: List[Int] = nearbyTickets.flatMap(ns => ns)
+    ns.filter(n => !rs.exists(r => n >= r._1 && n <= r._2)).sum
   }
 
   def solvePart2(lines: List[String]): Long = {
