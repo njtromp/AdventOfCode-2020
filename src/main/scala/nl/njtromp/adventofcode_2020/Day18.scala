d34e9f97d5c2fe13a6436a512ecd9ad5df97ba5d
Nico Tromp <nico.tromp@gmail.com>
2020-12-18 13:15:12 +0100

    Cleanup.

--- src/main/scala/nl/njtromp/adventofcode_2020/Day18.scala
+++ src/main/scala/nl/njtromp/adventofcode_2020/Day18.scala
@@ -3,24 +3,7 @@ package nl.njtromp.adventofcode_2020
 import scala.util.parsing.combinator.RegexParsers
 
 class Day18 extends Puzzle with RegexParsers {
-
-//  def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
-//  def factor: Parser[Double] = number | "(" ~> expr <~ ")"
-//  def term  : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
-//    case number ~ list => (number /: list) {
-//      case (x, "*" ~ y) => x * y
-//      case (x, "/" ~ y) => x / y
-//    }
-//  }
-//  def expr  : Parser[Double] = term ~ rep("+" ~ log(term)("Plus term") | "-" ~ log(term)("Minus term")) ^^ {
-//    case number ~ list => list.foldLeft(number) { // same as before, using alternate name for /:
-//      case (x, "+" ~ y) => x + y
-//      case (x, "-" ~ y) => x - y
-//    }
-//  }
-
-
-
+  def number: Parser[Long] = "\\d+".r ^^ {_.toLong}
 
   def solvePart1(lines: List[String]): Long = {
     lines.map(eval1).sum
@@ -31,7 +14,6 @@ class Day18 extends Puzzle with RegexParsers {
   }
 
   private def eval1(expr: String): Long = {
-    def number: Parser[Long] = "\\d+".r ^^ {_.toLong}
     def factor: Parser[Long] = number | "(" ~> expression <~ ")"
     def expression: Parser[Long] = factor ~ rep( ("*" | "+") ~ factor) ^^ {
       case number ~ list => (number /: list) {
@@ -43,11 +25,10 @@ class Day18 extends Puzzle with RegexParsers {
   }
 
   private def eval2(expr: String): Long = {
-    def number: Parser[Long] = "\\d+".r ^^ {_.toLong}
     def factor: Parser[Long] = number | "(" ~> expression <~ ")"
     def term: Parser[Long] = factor ~ rep( "+" ~ factor) ^^ {
       case number ~ list => (number /: list) { case (x, "+" ~ y) => x + y
-      case (x, "*" ~ y) => x * y
+      case (x, "+" ~ y) => x * y
       }
     }
     def expression: Parser[Long] = term ~ rep( "*" ~ term) ^^ {
