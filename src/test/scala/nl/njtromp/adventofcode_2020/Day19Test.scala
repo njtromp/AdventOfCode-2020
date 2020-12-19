fe73b3374115138cea39f781b265ec487c297c64
Nico Tromp <nico.tromp@gmail.com>
2020-12-19 07:50:20 +0100

    Ready for puzzle 19.

--- src/test/scala/nl/njtromp/adventofcode_2020/Day19Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2020/Day19Test.scala
@@ -4,10 +4,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class Day19Test extends AnyFlatSpec {
   val example = List(
+  "0: 4 1 5",
+  "1: 2 3 | 3 2",
+  "2: 4 4 | 5 5",
+  "3: 4 5 | 5 4",
+  "4: a",
+  "5: b",
+  "",
+  "ababbb",
+  "bababa",
+  "abbbab",
+  "aaabbb",
+  "aaaabbb"
   )
 
   "Part 1" should "solve example correct" in {
-    assert(new Day19().solvePart1(example) === 0)
+    assert(new Day19().solvePart1(example) === 2)
   }
 
   "Part 2" should "solve example correct" in {
