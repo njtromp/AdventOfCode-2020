0a7aac3245205b57fba96f8967dbbe90660f3478
Nico Tromp <nico.tromp@gmail.com>
2021-12-03 10:29:30 +0100

    Day 2 part 1 complete.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day02.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day02.scala
@@ -4,7 +4,16 @@ import nl.njtromp.adventofcode_2020.Puzzle
 
 class Day02 extends Puzzle {
 
-  override def solvePart1(lines: List[String]): Long = ???
+  override def solvePart1(lines: List[String]): Long = {
+    val pos = lines.map(_.split(" ")).map(c => {
+      c.head.toLowerCase() match {
+        case "forward" => (c(1).toInt, 0)
+        case "up" => (0, -c(1).toInt)
+        case "down" => (0, c(1).toInt)
+      }
+    }).reduce((a, b) => (a._1 + b._1, a._2 + b._2))
+    pos._1 * pos._2
+  }
 
   override def solvePart2(lines: List[String]): Long = ???
 
--- src/test/scala/nl/njtromp/adventofcode_2021/Day02Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day02Test.scala
@@ -9,11 +9,11 @@ class Day02Test extends AnyFlatSpec {
   val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day02-test.txt")).getLines().toList
 
   "Part 1" should "solve example correct" in {
-    assert(new Day01().solvePart1(example) === 0)
+    assert(new Day02().solvePart1(example) === 150)
   }
 
   "Part 2" should "solve example correct" in {
-    assert(new Day01().solvePart2(example) === 0)
+    assert(new Day02().solvePart2(example) === 0)
   }
 
 
