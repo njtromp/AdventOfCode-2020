60956e96b501f6aef1608f6139e144abfe967d0e
Nico Tromp <nico.tromp@gmail.com>
2021-12-04 09:20:17 +0100

    Generalize day 3.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day03.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day03.scala
@@ -9,7 +9,7 @@ class Day03 extends Puzzle {
 
   override def solvePart1(lines: List[String]): Long = {
     val decoded = lines.map(_.toCharArray.map(decodeBit))
-    val combined = decoded.reduce((a, b) => a.zip(b).map((x) => x._1 + x._2))
+    val combined = decoded.reduce((a, b) => a.zip(b).map(x => x._1 + x._2))
     val gammaRate = combined.foldLeft(0)((v, b) => v * 2 + (if (b > 0) 1 else 0))
     val epsilonRate = combined.foldLeft(0)((v, b) => v * 2 + (if (b < 0) 1 else 0))
     gammaRate * epsilonRate
@@ -18,32 +18,21 @@ class Day03 extends Puzzle {
   override def solvePart2(lines: List[String]): Long = {
     val decoded = lines.map(_.toCharArray.map(decodeBit))
     @tailrec
-    def o2Rating(v: Int, decoded: List[Array[Int]]): Long = {
+    def rating(v: Int, decoded: List[Array[Int]], f: List[Array[Int]] => Int): Long = {
       if (decoded.size == 1) {
         if (decoded.head.length == 0) {
           v
         } else {
-          o2Rating(v * 2 + (if (decoded.head.head == 1) 1 else 0), List(decoded.head.tail))
+          rating(v * 2 + (if (decoded.head.head == 1) 1 else 0), List(decoded.head.tail), f)
         }
       } else {
-        val bit = if (decoded.foldLeft(0)((v, b) => v + b.head) >= 0) 1 else -1;
-        o2Rating(v * 2 + (if (bit == 1) 1 else 0), decoded.filter(_.head == bit).map(_.tail))
+        val bit = f(decoded)
+        rating(v * 2 + (if (bit == 1) 1 else 0), decoded.filter(_.head == bit).map(_.tail), f)
       }
     }
-    @tailrec
-    def co2Rating(v: Int, decoded: List[Array[Int]]): Long = {
-      if (decoded.size == 1) {
-        if (decoded.head.length == 0) {
-          v
-        } else {
-          co2Rating(v * 2 + (if (decoded.head.head == 1) 1 else 0), List(decoded.head.tail))
-        }
-      } else {
-        val bit = if (decoded.foldLeft(0)((v, b) => v + b.head) >= 0) -1 else 1;
-        co2Rating(v * 2 + (if (bit == 1) 1 else 0), decoded.filter(_.head == bit).map(_.tail))
-      }
-    }
-    o2Rating(0, decoded) * co2Rating(0, decoded)
+    def o2Rating(x: List[Array[Int]]) = if (x.foldLeft(0)((v, b) => v + b.head) >= 0) 1 else -1
+    def co2Rating(x: List[Array[Int]]) = if (x.foldLeft(0)((v, b) => v + b.head) >= 0) -1 else 1
+    rating(0, decoded, o2Rating) * rating(0, decoded, co2Rating)
   }
 }
 
