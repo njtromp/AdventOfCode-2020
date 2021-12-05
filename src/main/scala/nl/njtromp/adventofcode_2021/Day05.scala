2c58ca7603925cfcf0e43081823300da075d7b03
Nico Tromp <nico.tromp@gmail.com>
2021-12-05 09:40:03 +0100

    Day 5 part 1 & 2 complete.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day05.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day05.scala
@@ -3,10 +3,47 @@ package nl.njtromp.adventofcode_2021
 import nl.njtromp.adventofcode_2020.Puzzle
 
 class Day05 extends Puzzle {
+  type Pos = (Int, Int)
 
-  override def solvePart1(lines: List[String]): Long = ???
+  private def position(p: String): Pos = (p.split(",")(0).trim.toInt, p.split(",")(1).trim.toInt)
 
-  override def solvePart2(lines: List[String]): Long = ???
+  private def beginEndPoint(line: String): (Pos, Pos) = {
+    val p = line.split("->")
+    (position(p(0)), position(p(1)))
+  }
+
+  private def coveringPositions(p: (Pos, Pos)): List[Pos] = {
+    def range(a: Int, b: Int): Range =
+      if (a > b) (b to a) else (a to b)
+    val b = p._1
+    val e = p._2
+    if (b._1 == e._1)
+      range(b._2, e._2).map((b._1, _)).toList
+    else if (b._2 == e._2)
+      range(b._1, e._1).map((_, b._2)).toList
+    else {
+      (b._1 to e._1 by Math.signum(e._1 - b._1).toInt).toList
+      .zip((b._2 to e._2 by Math.signum(e._2 - b._2).toInt).toList)
+    }
+  }
+
+  override def solvePart1(lines: List[String]): Long = {
+    val lineInfo = lines.map(beginEndPoint)
+    val vhLines = lineInfo.filter(l => l._1._1 == l._2._1 || l._1._2 == l._2._2)
+    val points = vhLines.flatMap(coveringPositions)
+    points.foldLeft(Map.empty[Pos, Int]) { (m, c) =>
+      m + (c -> (m.getOrElse(c, 0) + 1))
+    }.count(_._2 > 1)
+  }
+
+  override def solvePart2(lines: List[String]): Long = {
+    val lineInfo = lines.map(beginEndPoint)
+    val points = lineInfo.flatMap(coveringPositions)
+    points.foldLeft(Map.empty[Pos, Int]) { (m, c) =>
+      m + (c -> (m.getOrElse(c, 0) + 1))
+    }.count(_._2 > 1)
+
+  }
 }
 
 object Day05 extends App {
--- src/test/scala/nl/njtromp/adventofcode_2021/Day05Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day05Test.scala
@@ -8,11 +8,11 @@ class Day05Test extends AnyFlatSpec {
   val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day05-example.txt")).getLines().toList
 
   "Part 1" should "solve example correct" in {
-    assert(new Day05().solvePart1(example) === 4512)
+    assert(new Day05().solvePart1(example) === 5)
   }
 
   "Part 2" should "solve example correct" in {
-    assert(new Day05().solvePart2(example) === 1924)
+    assert(new Day05().solvePart2(example) === 12)
   }
 
 
