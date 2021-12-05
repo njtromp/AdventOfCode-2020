e05c594af076024460ea5f3e824ea615905fe31c
Nico Tromp <nico.tromp@gmail.com>
2021-12-05 09:53:13 +0100

    Prepare for day 6.

---
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day06.scala
@@ -0,0 +1,14 @@
+package nl.njtromp.adventofcode_2021
+
+import nl.njtromp.adventofcode_2020.Puzzle
+
+class Day06 extends Puzzle{
+
+  override def solvePart1(lines: List[String]): Long = ???
+
+  override def solvePart2(lines: List[String]): Long = ???
+}
+
+object Day06 extends App {
+  new Day06().solvePuzzles("/2021/day06.txt")
+}
