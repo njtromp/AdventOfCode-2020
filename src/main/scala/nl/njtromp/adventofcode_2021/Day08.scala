f100e54f78b0ca82609193ab11cd95f59943f241
Nico Tromp <nico.tromp@gmail.com>
2021-12-07 10:55:39 +0100

    Prepare for day 8.

---
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day08.scala
@@ -0,0 +1,14 @@
+package nl.njtromp.adventofcode_2021
+
+import nl.njtromp.adventofcode.Puzzle
+
+class Day08 extends Puzzle {
+
+  override def solvePart1(lines: List[String]): Long = ???
+
+  override def solvePart2(lines: List[String]): Long = ???
+}
+
+object Day08 extends App{
+  new Day08().solvePuzzles("/2021/day08.txt")
+}
---
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day08Test.scala
@@ -0,0 +1,20 @@
+package nl.njtromp.adventofcode_2021
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.io.Source
+
+
+class Day08Test extends AnyFlatSpec {
+  val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day08-example.txt")).getLines().toList
+
+  "Part 1" should "solve example correct" in {
+    assert(new Day08().solvePart1(example) === 37)
+  }
+
+  "Part 2" should "solve example correct" in {
+    assert(new Day08().solvePart2(example) === 168)
+  }
+
+
+}
