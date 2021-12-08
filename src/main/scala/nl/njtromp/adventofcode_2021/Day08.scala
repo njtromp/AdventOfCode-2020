b0bcc70d06ce039b6a948450ce7daa08a981e440
Nico Tromp <nico.tromp@gmail.com>
2021-12-08 06:42:56 +0100

    Part 1 of day 8 complete.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day08.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day08.scala
@@ -4,7 +4,10 @@ import nl.njtromp.adventofcode.Puzzle
 
 class Day08 extends Puzzle {
 
-  override def solvePart1(lines: List[String]): Long = ???
+  override def solvePart1(lines: List[String]): Long = {
+    val bla = lines.map(_.split("\\|")(1).split(" ").map(_.trim).filterNot(_.isBlank))
+    bla.flatMap(_.map(_.length)).count(List(2, 4, 3, 7).contains(_))
+  }
 
   override def solvePart2(lines: List[String]): Long = ???
 }
--- src/test/scala/nl/njtromp/adventofcode_2021/Day08Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day08Test.scala
@@ -9,11 +9,11 @@ class Day08Test extends AnyFlatSpec {
   val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day08-example.txt")).getLines().toList
 
   "Part 1" should "solve example correct" in {
-    assert(new Day08().solvePart1(example) === 0)
+    assert(new Day08().solvePart1(example) === 26)
   }
 
   "Part 2" should "solve example correct" in {
-    assert(new Day08().solvePart2(example) === 0)
+    assert(new Day08().solvePart2(example) === 61229)
   }
 
 
