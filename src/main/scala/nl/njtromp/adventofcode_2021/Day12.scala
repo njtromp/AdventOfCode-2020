8d729332688e03763b30627a46edc671c7011fd2
Nico Tromp <nico.tromp@gmail.com>
2021-12-12 10:38:08 +0100

    Use single method for finding paths and use val-naming that indicate the meaning of its content.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day12.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day12.scala
@@ -7,27 +7,17 @@ class Day12 extends Puzzle {
   case class SmallCave(name: String) extends Cave
   case class LargeCave(name: String) extends Cave
 
-  def createConnection(line: String): (Cave, Cave) = {
+  def createEdge(line: String): (Cave, Cave) = {
     def createCave(name: String): Cave =
       if (name.toUpperCase == name) LargeCave(name) else SmallCave(name)
     val names = line.split("-")
     (createCave(names(0)), createCave(names(1)))
   }
 
-  def countPaths(current: Cave, finish: Cave, visited: Set[Cave], adjLists: Map[Cave, List[Cave]]): Long = {
-    if (current == finish)
-      1L
-    else
-      current match {
-        case SmallCave(_) => adjLists(current).filterNot(visited.contains).map(c => countPaths(c, finish, visited + current, adjLists)).sum
-        case LargeCave(_) => adjLists(current).filterNot(visited.contains).map(c => countPaths(c, finish, visited, adjLists)).sum
-      }
-  }
-
   override def solvePart1(lines: List[String]): Long = {
-    val structure = lines.map(createConnection)
-    val adjLists = structure.flatMap(s => List(s._1 -> s._2, s._2 -> s._1)).groupBy(_._1).mapValues(ds => ds.map(_._2))
-    countPaths(SmallCave("start"), SmallCave("end"), Set.empty[Cave], adjLists)
+    val edges = lines.map(createEdge)
+    val adjLists = edges.flatMap(s => List(s._1 -> s._2, s._2 -> s._1)).groupBy(_._1).mapValues(ds => ds.map(_._2))
+    createPaths(List(SmallCave("start")), SmallCave("end"), None, Set.empty[Cave], adjLists).size
   }
 
   def createPaths(path: List[Cave], finish: Cave, canVisitTwice: Option[Cave], visited: Set[Cave], adjLists: Map[Cave, List[Cave]]): List[List[Cave]] = {
@@ -47,12 +37,12 @@ class Day12 extends Puzzle {
     }
   }
   override def solvePart2(lines: List[String]): Long = {
-    val structure = lines.map(createConnection)
-    val smallCaves = structure.flatMap(s => List(s._1, s._2)).toSet.filter(c => c match {
+    val edges = lines.map(createEdge)
+    val smallCaves = edges.flatMap(s => List(s._1, s._2)).toSet.filter(c => c match {
       case SmallCave(_) => true
       case LargeCave(_) => false
     }).filterNot(List(SmallCave("start"), SmallCave("end")).contains).toList
-    val adjLists = structure.flatMap(s => List(s._1 -> s._2, s._2 -> s._1)).groupBy(_._1).mapValues(ds => ds.map(_._2))
+    val adjLists = edges.flatMap(s => List(s._1 -> s._2, s._2 -> s._1)).groupBy(_._1).mapValues(ds => ds.map(_._2))
     smallCaves.flatMap(c => createPaths(List(SmallCave("start")), SmallCave("end"), Some(c), Set.empty[Cave], adjLists)).toSet.size
   }
 }
