c80ce50cc992df73d5e0e9038f40af1a071688cc
Nico Tromp <nico.tromp@gmail.com>
2021-12-12 11:21:35 +0100

    Add timing info to output.

--- src/main/scala/nl/njtromp/adventofcode/Puzzle.scala
+++ src/main/scala/nl/njtromp/adventofcode/Puzzle.scala
@@ -1,16 +1,25 @@
 package nl.njtromp.adventofcode
 
+import java.util.concurrent.TimeUnit
 import scala.io.Source
 
 trait Puzzle {
 
   def solvePuzzles(inputName: String): Unit = {
     val lines: List[String] = Source.fromInputStream(getClass.getResourceAsStream(inputName)).getLines().toList
-    println(s"Answer ${getClass.getSimpleName} part 1: ${solvePart1(lines)}")
-    println(s"Answer ${getClass.getSimpleName} part 2: ${solvePart2(lines)}")
+    println(s"Answer ${getClass.getSimpleName} part 1: ${time{solvePart1(lines)}}")
+    println(s"Answer ${getClass.getSimpleName} part 2: ${time{solvePart2(lines)}}")
   }
 
-  def solvePart1(lines: List[String]): Long;
+  def solvePart1(lines: List[String]): Long
 
-  def solvePart2(lines: List[String]): Long;
+  def solvePart2(lines: List[String]): Long
+
+  def time[R](block: => R): R = {
+    val start = System.nanoTime()
+    val result = block    // call-by-name
+    val finish = System.nanoTime()
+    println(f"Elapsed time: ${TimeUnit.NANOSECONDS.toMillis(finish - start)}%,d ms")
+    result
+  }
 }
