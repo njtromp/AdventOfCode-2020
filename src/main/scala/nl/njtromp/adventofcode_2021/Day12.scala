b55f7678b20f9361ada4fedd2ab607a0ef8b3210
Nico Tromp <nico.tromp@gmail.com>
2021-12-12 11:19:20 +0100

    Use matching on class instead of on value.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day12.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day12.scala
@@ -14,35 +14,35 @@ class Day12 extends Puzzle {
     (createCave(names(0)), createCave(names(1)))
   }
 
+  def createPaths(path: List[Cave], finish: Cave, canVisitTwice: Option[Cave], visited: Set[Cave], adjLists: Map[Cave, List[Cave]]): List[List[Cave]] = {
+    if (path.head == finish)
+      List(path)
+    else {
+      (path.head, canVisitTwice) match {
+        case (current: SmallCave, None) => adjLists(current).filterNot(visited.contains).flatMap(c => createPaths(c :: path, finish, None, visited + current, adjLists))
+        case (current: SmallCave, Some(twice)) =>
+          if (current == twice)
+            adjLists(current).filterNot(visited.contains).flatMap(c => createPaths(c :: path, finish, None, visited, adjLists))
+          else
+            adjLists(current).filterNot(visited.contains).flatMap(c => createPaths(c :: path, finish, canVisitTwice, visited + current, adjLists))
+        case (current: LargeCave, _) => adjLists(current).filterNot(visited.contains).flatMap(c => createPaths(c :: path, finish, canVisitTwice, visited, adjLists))
+      }
+    }
+  }
+
   override def solvePart1(lines: List[String]): Long = {
     val edges = lines.map(createEdge)
     val adjLists = edges.flatMap(s => List(s._1 -> s._2, s._2 -> s._1)).groupBy(_._1).mapValues(ds => ds.map(_._2))
     createPaths(List(SmallCave("start")), SmallCave("end"), None, Set.empty[Cave], adjLists).size
   }
 
-  def createPaths(path: List[Cave], finish: Cave, canVisitTwice: Option[Cave], visited: Set[Cave], adjLists: Map[Cave, List[Cave]]): List[List[Cave]] = {
-    if (path.head == finish)
-      List(path)
-    else {
-      val current = path.head
-      (current, canVisitTwice) match {
-        case (SmallCave(_), None) => adjLists(current).filterNot(visited.contains).flatMap(c => createPaths(c :: path, finish, None, visited + current, adjLists))
-        case (SmallCave(_), Some(twice)) =>
-          if (current == twice)
-            adjLists(current).filterNot(visited.contains).flatMap(c => createPaths(c :: path, finish, None, visited, adjLists))
-          else
-            adjLists(current).filterNot(visited.contains).flatMap(c => createPaths(c :: path, finish, canVisitTwice, visited + current, adjLists))
-        case (LargeCave(_), _) => adjLists(current).filterNot(visited.contains).flatMap(c => createPaths(c :: path, finish, canVisitTwice, visited, adjLists))
-      }
-    }
-  }
   override def solvePart2(lines: List[String]): Long = {
     val edges = lines.map(createEdge)
+    val adjLists = edges.flatMap(s => List(s._1 -> s._2, s._2 -> s._1)).groupBy(_._1).mapValues(ds => ds.map(_._2))
     val smallCaves = edges.flatMap(s => List(s._1, s._2)).toSet.filter(c => c match {
       case SmallCave(_) => true
       case LargeCave(_) => false
     }).filterNot(List(SmallCave("start"), SmallCave("end")).contains).toList
-    val adjLists = edges.flatMap(s => List(s._1 -> s._2, s._2 -> s._1)).groupBy(_._1).mapValues(ds => ds.map(_._2))
     smallCaves.flatMap(c => createPaths(List(SmallCave("start")), SmallCave("end"), Some(c), Set.empty[Cave], adjLists)).toSet.size
   }
 }
