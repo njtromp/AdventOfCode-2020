617780498cb255c64dc92a6427747b217356c9db
Nico Tromp <nico.tromp@gmail.com>
2021-12-14 23:23:14 +0100

    Prepare for day 15.

---
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day15.scala
@@ -0,0 +1,14 @@
+package nl.njtromp.adventofcode_2021
+
+import nl.njtromp.adventofcode.Puzzle
+
+class Day15 extends Puzzle {
+
+  override def solvePart1(lines: List[String]): Long = ???
+
+  override def solvePart2(lines: List[String]): Long = ???
+}
+
+object Day15 extends App {
+  new Day15().solvePuzzles("/2021/day15.txt")
+}
---
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day15Test.scala
@@ -0,0 +1,18 @@
+package nl.njtromp.adventofcode_2021
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.io.Source
+
+class Day15Test extends AnyFlatSpec {
+  val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day15-example.txt")).getLines().toList
+
+  "Part 1" should "solve example correct" in {
+    assert(new Day15().solvePart1(example) === 0) // Some oddities with 'B' count
+  }
+
+  "Part 2" should "solve example correct" in {
+    assert(new Day15().solvePart2(example) === 0)
+  }
+
+}
