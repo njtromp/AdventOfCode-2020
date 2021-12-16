310d2f9fda6f95781540b73cf1de632c8ef05eeb
Nico Tromp <nico.tromp@gmail.com>
2021-12-16 17:16:45 +0100

    Prepare for day 17.

---
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day17.scala
@@ -0,0 +1,10 @@
+package nl.njtromp.adventofcode_2021
+
+import nl.njtromp.adventofcode.Puzzle
+
+class Day17 extends Puzzle {
+
+  override def solvePart1(lines: List[String]): Long = ???
+
+  override def solvePart2(lines: List[String]): Long = ???
+}
