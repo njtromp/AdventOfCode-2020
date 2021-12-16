5062586b298ebb72d9e88917705a3d20c76fa0e8
Nico Tromp <nico.tromp@gmail.com>
2021-12-16 17:11:57 +0100

    Part 2 for day 16 complete.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day16.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day16.scala
@@ -7,16 +7,28 @@ class Day16 extends Puzzle {
   def parseHex(c: Char): Int = Integer.parseInt(s"$c", 16)
   def parseHex(s: String): Int = Integer.parseInt(s, 16)
   def parseBin(s: String): Int = Integer.parseInt(s, 2)
+  def parseBigBin(s: String): BigInt = s.toCharArray.foldLeft(BigInt.apply(0))((acc, c) => acc * BigInt.apply(2) + (if (c == '1') BigInt.apply(1) else BigInt.apply(0)))
   def toBinString(d: Int): String = Integer.toString(d, 2)
 
   sealed abstract class Packet {
-    def bits(): Int
+    def bits: Int
+    def value: BigInt
   }
   case class Literal(version: Int, values: List[String]) extends Packet {
-    def bits(): Int = 3 + 3 + (1 + 4) * values.length;
+    def bits: Int = 3 + 3 + (1 + 4) * values.length
+    def value: BigInt = parseBigBin(values.mkString)
   }
-  case class Operation(version: Int, length: Int, subPackets: List[Packet]) extends Packet {
-    def bits(): Int = 3 + 3 + 1 + length + subPackets.map(_.bits()).sum
+  case class Operation(version: Int, opCode: Int, length: Int, subPackets: List[Packet]) extends Packet {
+    def bits: Int = 3 + 3 + 1 + length + subPackets.map(_.bits).sum
+    def value: BigInt = opCode match {
+      case 0 => subPackets.map(_.value).sum
+      case 1 => subPackets.map(_.value).product
+      case 2 => subPackets.map(_.value).min
+      case 3 => subPackets.map(_.value).max
+      case 5 => subPackets.map(_.value).reduce((a, b) => if (a > b) 1 else 0)
+      case 6 => subPackets.map(_.value).reduce((a, b) => if (a < b) 1 else 0)
+      case 7 => subPackets.map(_.value).reduce((a, b) => if (a == b) 1 else 0)
+    }
   }
 
   def decodeLiteral(encodedMessage: String): List[String] = {
@@ -32,19 +44,19 @@ class Day16 extends Puzzle {
     val message = encodedMessage.drop(6)
     typeId match {
       case 4 =>
-        val literals = decodeLiteral(message).reverse
+        val literals = decodeLiteral(message)
         Literal(version, literals)
       case _ =>
         message.head match {
           case '0' =>
             val packetSize = parseBin(message.slice(1,16))
-            Operation(version, 15, decodePackets(message.slice(16, 16 + packetSize)))
+            Operation(version, typeId, 15, decodePackets(message.slice(16, 16 + packetSize)))
           case '1' =>
             val numberOfPackets = parseBin(message.slice(1, 12))
             val packets = (1 to numberOfPackets).foldLeft(List.empty[Packet])((acc, _) =>
-              decodePacket(message.drop(12 + acc.map(_.bits()).sum)) :: acc
-            )
-            Operation(version, 11, packets)
+              decodePacket(message.drop(12 + acc.map(_.bits).sum)) :: acc
+            ).reverse
+            Operation(version, typeId, 11, packets)
         }
     }
   }
@@ -54,14 +66,14 @@ class Day16 extends Puzzle {
       List.empty[Packet]
     } else {
       val packet = decodePacket(encodedMessage)
-      packet :: decodePackets(encodedMessage.drop(packet.bits()))
+      packet :: decodePackets(encodedMessage.drop(packet.bits))
     }
   }
 
   def sumVersionNumbers(p: Packet): Int = {
     p match {
       case Literal(v, _) => v
-      case Operation(v, _, s) => v + s.map(sumVersionNumbers).sum
+      case Operation(v, _, _, s) => v + s.map(sumVersionNumbers).sum
     }
   }
 
@@ -71,7 +83,11 @@ class Day16 extends Puzzle {
     sumVersionNumbers(decodedPacket)
   }
 
-  override def solvePart2(lines: List[String]): Long = ???
+  override def solvePart2(lines: List[String]): Long = {
+    val bitEncodedMessage = lines.head.map(parseHex).map(toBinString).map("000" + _ takeRight 4).toList.mkString
+    val decodedPacket = decodePacket(bitEncodedMessage)
+    decodedPacket.value.longValue()
+  }
 }
 
 object Day16 extends App {
--- src/test/scala/nl/njtromp/adventofcode_2021/Day16Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day16Test.scala
@@ -9,6 +9,7 @@ class Day16Test extends AnyFlatSpec {
   val example_1_1: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day16-example-1-1.txt")).getLines().toList
   val example_1_2: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day16-example-1-2.txt")).getLines().toList
   val example_1_3: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day16-example-1-3.txt")).getLines().toList
+  val example_2: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day16-example-2.txt")).getLines().toList
 
   "Part 1" should "solve example 0 correct" in {
     assert(new Day16().solvePart1(example_1_0) === 6)
@@ -27,7 +28,10 @@ class Day16Test extends AnyFlatSpec {
   }
 
   "Part 2" should "solve example correct" in {
-    assert(new Day16().solvePart2(example_1_1) === 0)
+    example_2.map(_.split(" ")).foreach(ms =>
+      assert(new Day16().solvePart2(List(ms(0))) === ms(1).toLong)
+    )
+
   }
 
 }
