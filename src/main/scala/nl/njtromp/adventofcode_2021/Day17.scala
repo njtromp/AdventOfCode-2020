522fc8422d11a599aef7c0adb4f7b8ebdd7c0f97
Nico Tromp <nico.tromp@gmail.com>
2021-12-17 07:19:15 +0100

    Part 1 of day 17 complete.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day17.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day17.scala
@@ -3,8 +3,42 @@ package nl.njtromp.adventofcode_2021
 import nl.njtromp.adventofcode.Puzzle
 
 class Day17 extends Puzzle {
+  type Pos = (Int, Int) // (X, Y)
+  type Velocity = (Int, Int) // (X direction, Y direction)
+  type TargetRange = (Range, Range)
 
-  override def solvePart1(lines: List[String]): Long = ???
+  private val TargetArea = "target area: x=(\\d+)..(\\d+), y=-(\\d+)..-(\\d+)".r // (X-range, Y-range)
+
+  def isOnTarget(p: Pos, t: TargetRange): Boolean = t._1.contains(p._1) && t._2.contains(p._2)
+  def hasOverShoot(p: Pos, t: TargetRange): Boolean = p._1 > t._1.max || p._2 < t._2.min
+
+  def drag(x: Int): Int = if (x > 0) x - 1 else if (x < 0) x + 1 else 0
+  def oneStep(p: Pos, v: Velocity): (Pos, Velocity) =
+    ((p._1 + v._1, p._2 + v._2), (drag(v._1), v._2 - 1))
+
+  def shootProbe(v: Velocity, t: TargetRange): List[Pos] = {
+    def shootProbe(p: Pos, v: Velocity): List[Pos] = {
+      val (pos, velocity) = oneStep(p, v)
+      if (isOnTarget(pos, t))
+        List(pos)
+      else {
+        if (!hasOverShoot(pos, t))
+        pos :: shootProbe(pos, velocity)
+      else
+        List.empty
+      }
+    }
+    val trajectory = shootProbe((0, 0), v)
+    if (trajectory.nonEmpty && isOnTarget(trajectory.reverse.head, t)) trajectory else List.empty
+  }
+
+  override def solvePart1(lines: List[String]): Long = {
+    val targetArea = lines.map({
+      case TargetArea(minX, maxX, minY, maxY) => (minX.toInt to maxX.toInt, -minY.toInt to -maxY.toInt)
+    }).head
+    val trajectories = (1 to 100).flatMap(x => (-50  to 500).map(y => shootProbe((x, y), targetArea)))
+    trajectories.filterNot(_.isEmpty).map(_.maxBy(_._2)._2).max
+  }
 
   override def solvePart2(lines: List[String]): Long = ???
 }
--- src/test/scala/nl/njtromp/adventofcode_2021/Day17Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day17Test.scala
@@ -8,7 +8,7 @@ class Day17Test extends AnyFlatSpec {
   val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day17-example.txt")).getLines().toList
 
   "Part 1" should "solve example correct" in {
-    assert(new Day17().solvePart1(example) === 0)
+    assert(new Day17().solvePart1(example) === 45)
   }
 
   "Part 2" should "solve example correct" in {
