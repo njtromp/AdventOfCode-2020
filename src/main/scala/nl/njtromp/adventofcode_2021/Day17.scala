336eb2e44dbe6c2f64a2274997ddb7ffa704d777
Nico Tromp <nico.tromp@gmail.com>
2021-12-17 16:56:00 +0100

    Minor cleanup.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day17.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day17.scala
@@ -4,14 +4,13 @@ import nl.njtromp.adventofcode.Puzzle
 
 class Day17 extends Puzzle {
   type Pos = (Int, Int) // (X, Y)
-  type Velocity = (Int, Int) // (X direction, Y direction)
-  type TargetRange = (Range, Range)
+  type Velocity = (Int, Int) // (X-direction, Y-direction)
+  type TargetRange = (Range, Range) // (X-range, Y-range)
 
-  private val TargetArea = "target area: x=(\\d+)..(\\d+), y=-(\\d+)..-(\\d+)".r // (X-range, Y-range)
+  private val TargetArea = "target area: x=(\\d+)..(\\d+), y=(-\\d+)..(-\\d+)".r // (X-range, Y-range)
 
   def isOnTarget(p: Pos, t: TargetRange): Boolean = t._1.contains(p._1) && t._2.contains(p._2)
   def hasOverShoot(p: Pos, t: TargetRange): Boolean = p._1 > t._1.max || p._2 < t._2.min
-
   def drag(x: Int): Int = if (x > 0) x - 1 else if (x < 0) x + 1 else 0
   def oneStep(p: Pos, v: Velocity): (Pos, Velocity) = ((p._1 + v._1, p._2 + v._2), (drag(v._1), v._2 - 1))
 
@@ -20,12 +19,10 @@ class Day17 extends Puzzle {
       val (pos, velocity) = oneStep(p, v)
       if (isOnTarget(pos, t))
         List(pos)
-      else {
-        if (!hasOverShoot(pos, t))
+      else if (!hasOverShoot(pos, t))
         pos :: shootProbe(pos, velocity)
       else
         List.empty
-      }
     }
     val trajectory = shootProbe((0, 0), v)
     if (trajectory.nonEmpty && isOnTarget(trajectory.reverse.head, t)) trajectory else List.empty
@@ -33,15 +30,15 @@ class Day17 extends Puzzle {
 
   override def solvePart1(lines: List[String]): Long = {
     val targetArea = lines.map({
-      case TargetArea(minX, maxX, minY, maxY) => (minX.toInt to maxX.toInt, -minY.toInt to -maxY.toInt)
+      case TargetArea(minX, maxX, minY, maxY) => (minX.toInt to maxX.toInt, minY.toInt to maxY.toInt)
     }).head
     val trajectories = (1 to 100).flatMap(x => (-50  to 500).map(y => shootProbe((x, y), targetArea)))
-    trajectories.filterNot(_.isEmpty).map(_.maxBy(_._2)._2).max
+    trajectories.filter(_.nonEmpty).map(_.maxBy(_._2)._2).max
   }
 
   override def solvePart2(lines: List[String]): Long = {
     val targetArea = lines.map({
-      case TargetArea(minX, maxX, minY, maxY) => (minX.toInt to maxX.toInt, -minY.toInt to -maxY.toInt)
+      case TargetArea(minX, maxX, minY, maxY) => (minX.toInt to maxX.toInt, minY.toInt to maxY.toInt)
     }).head
     val trajectories = (1 to 200).flatMap(x => (-1000 to 1000).map(y => shootProbe((x, y), targetArea)))
     trajectories.count(_.nonEmpty)
