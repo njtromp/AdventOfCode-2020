2718506bef7f3d3ca2358e506ab5406453d0789c
Nico Tromp <nico.tromp@gmail.com>
2021-12-17 07:33:34 +0100

    Part 2 of day 17 complete.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day17.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day17.scala
@@ -13,8 +13,7 @@ class Day17 extends Puzzle {
   def hasOverShoot(p: Pos, t: TargetRange): Boolean = p._1 > t._1.max || p._2 < t._2.min
 
   def drag(x: Int): Int = if (x > 0) x - 1 else if (x < 0) x + 1 else 0
-  def oneStep(p: Pos, v: Velocity): (Pos, Velocity) =
-    ((p._1 + v._1, p._2 + v._2), (drag(v._1), v._2 - 1))
+  def oneStep(p: Pos, v: Velocity): (Pos, Velocity) = ((p._1 + v._1, p._2 + v._2), (drag(v._1), v._2 - 1))
 
   def shootProbe(v: Velocity, t: TargetRange): List[Pos] = {
     def shootProbe(p: Pos, v: Velocity): List[Pos] = {
