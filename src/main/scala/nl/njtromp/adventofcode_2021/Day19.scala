7ba78b3737a08da0c3327e0bf62a4a45664feefc
Nico Tromp <nico.tromp@gmail.com>
2021-12-19 23:17:10 +0100

    Prepare for day 19.

---
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day19.scala
@@ -0,0 +1,14 @@
+package nl.njtromp.adventofcode_2021
+
+import nl.njtromp.adventofcode.Puzzle
+
+class Day19 extends Puzzle {
+
+  override def solvePart1(lines: List[String]): Long = ???
+
+  override def solvePart2(lines: List[String]): Long = ???
+}
+
+object Day19 extends App {
+  new Day19().solvePuzzles("/2021/day19.txt")
+}
