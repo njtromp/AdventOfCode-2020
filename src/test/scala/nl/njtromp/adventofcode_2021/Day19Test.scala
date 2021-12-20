9e1a8ffb11c190ff4b65791380e26a74a00601b1
Nico Tromp <nico.tromp@gmail.com>
2021-12-20 13:19:18 +0100

    Prepare for day 19.

--- src/test/scala/nl/njtromp/adventofcode_2021/Day19Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day19Test.scala
@@ -8,7 +8,7 @@ class Day19Test extends AnyFlatSpec {
   val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day19-example.txt")).getLines().toList
 
   "Part 1" should "solve example correct" in {
-    assert(new Day19().solvePart1(example) === 0)
+    assert(new Day19().solvePart1(example) === 79)
   }
 
   "Part 2" should "solve example correct" in {
