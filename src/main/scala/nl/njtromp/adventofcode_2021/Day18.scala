c0ae25d3f5d416162c2b8094f1cb8a9694a8c4b5
Nico Tromp <nico.tromp@gmail.com>
2021-12-20 10:58:25 +0100

    Removed unsed code and improve naming.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day18.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day18.scala
@@ -3,33 +3,28 @@ package nl.njtromp.adventofcode_2021
 import nl.njtromp.adventofcode.Puzzle
 
 import scala.annotation.tailrec
-import scala.collection.mutable
 import scala.util.parsing.combinator.RegexParsers
 
 class Day18 extends Puzzle with RegexParsers {
-  def leaf: Parser[Number] = "\\d+".r ^^ { n => Leaf(n.toLong)}
-  def node: Parser[Number] = "[" ~> (leaf|node) ~ "," ~ (leaf|node) <~ "]" ^^ {
+  def LEAF: Parser[Number] = "\\d+".r ^^ { n => Leaf(n.toLong)}
+  def NODE: Parser[Number] = "[" ~> (LEAF|NODE) ~ "," ~ (LEAF|NODE) <~ "]" ^^ {
     case l ~ _ ~ r => Node(l, r)
   }
 
-  // Don't be alarmed when the compiler warns about matches not being exhaustive on lines 92, 95, 104 and 127.
+  // Don't be alarmed when the compiler warns about matches not being exhaustive.
   // The possible cases the compiler 'sees' can't occur in real life.
   sealed abstract class Number {
-    def value: Long
     def isLeaf = false
     def canExplode = false
-    def parent(child: Number): Option[Number]
     def asList(exploding: Number): List[Number]
-    def replace(mapping: mutable.ArrayBuffer[(Number, Number)]): Number
+    def replace(mapping: List[(Number, Number)]): Number
     def split: Number
-    def explode: Number = explodeNested(this)
     def magnitude: Long
   }
   case class Leaf(value: Long) extends Number {
     override def isLeaf = true
-    override def parent(child: Number): Option[Number] = None
     override def asList(exploding: Number): List[Number] = List(this)
-    override def replace(mapping: mutable.ArrayBuffer[(Number, Number)]): Number = {
+    override def replace(mapping: List[(Number, Number)]): Number = {
       mapping.find(m => m._1.eq(this)) match {
         case None => this
         case Some(m) => m._2
@@ -43,20 +38,10 @@ class Day18 extends Puzzle with RegexParsers {
     override def toString: String = value.toString
   }
   case class Node(left: Number, right: Number) extends Number {
-    // A Node should never be asked for its value (for now), If it happens it indicates a major problem in the algorithm.
-    override def value: Long = ???
     override def canExplode: Boolean = left.isLeaf && right.isLeaf
-    override def parent(child: Number): Option[Number] = if (left.eq(child) || right.eq(child))
-      Some(this)
-    else {
-      left.parent(child) match {
-        case None => right.parent(child)
-        case Some(p) => Some(p)
-      }
-    }
     override def asList(exploding: Number): List[Number] =
       if (this.eq(exploding)) List(this) else left.asList(exploding) ++ right.asList(exploding)
-    override def replace(mapping: mutable.ArrayBuffer[(Number, Number)]): Number = {
+    override def replace(mapping: List[(Number, Number)]): Number = {
       mapping.find(m => m._1.eq(this)) match {
         case None => Node(left.replace(mapping), right.replace(mapping))
         case Some(m) => m._2
@@ -74,64 +59,67 @@ class Day18 extends Puzzle with RegexParsers {
     override def toString: String = s"[${left.toString},${right.toString}]"
   }
 
-  private def explodeNested(root: Number): Number = {
-    def toExplode(level: Int, node: Number): Option[Number] = {
+  private def explode(root: Number): Number = {
+    def findExploding(level: Int, node: Number): Option[Number] = {
       if (node.canExplode && level >= 4) {
         Some(node)
       } else {
         node match {
           case _: Leaf => None
-          case Node(l, r) => toExplode(level + 1, l) match {
-            case None => toExplode(level + 1, r)
+          case Node(l, r) => findExploding(level + 1, l) match {
+            case None => findExploding(level + 1, r)
             case Some(e) => Some(e)
           }
         }
       }
     }
     def explode(exploding: Number): Number = {
-      def leftValue(node: Number): Long = node match {
-        case Node(l, _) => l.value
+      def value(leaf: Number): Long = leaf match {
+        case Leaf(l) => l
       }
-      def rightValue(node: Number): Long = node match {
-        case Node(_, r) => r.value
+      def leftValue: Long = exploding match {
+        case Node(l, _) => value(l)
+      }
+      def rightValue: Long = exploding match {
+        case Node(_, r) => value(r)
       }
       // Get all the leafs and the exploding node in a list. The order is in pre-order. The Leaf
       // that can participate in the explosion is in front of the exploding node while the Leaf on the right
       // is the first one behind the exploding node.
       val values = root.asList(exploding)
-      val inFront = values.takeWhile(!_.eq(exploding)).reverse // In Reverse order for easy access the right-most number
-      val behind = values.dropWhile(!_.eq(exploding))
-      val mapping: mutable.ArrayBuffer[(Number, Number)] = (inFront.take(1).headOption, behind.tail.take(1).headOption) match {
-        case (None, Some(r)) => mutable.ArrayBuffer(
+      val before = values.takeWhile(!_.eq(exploding)).reverse // In Reverse order for easy access the right-most number
+      val after = values.dropWhile(!_.eq(exploding)).tail
+      val mapping: List[(Number, Number)] = (before.take(1).headOption, after.take(1).headOption) match {
+        case (None, Some(r)) => List(
           (exploding, Leaf(0)),
-          (r, Leaf(r.value + rightValue(exploding)))
+          (r, Leaf(value(r) + rightValue))
         )
-        case (Some(l), None) => mutable.ArrayBuffer(
-          (exploding, Leaf(0)),
-          (l, Leaf(l.value + leftValue(exploding)))
+        case (Some(l), None) => List(
+          (l, Leaf(value(l) + leftValue)),
+          (exploding, Leaf(0))
         )
-        case (Some(l), Some(r)) => mutable.ArrayBuffer(
+        case (Some(l), Some(r)) => List(
+          (l, Leaf(value(l) + leftValue)),
           (exploding, Leaf(0)),
-          (l, Leaf(l.value + leftValue(exploding))),
-          (r, Leaf(r.value + rightValue(exploding))))
+          (r, Leaf(value(r) + rightValue)))
       }
       root.replace(mapping)
     }
-    toExplode(0, root) match {
+    findExploding(0, root) match {
       case None => root
       case Some(e) => explode(e)
     }
   }
 
-  private def parseSnailfishNumber(snailfishNumber: String): Number = {
-    parse(node, snailfishNumber) match {
+  private def parse(snailfishNumber: String): Number = {
+    parse(NODE, snailfishNumber) match {
       case Success(n, _) => n
     }
   }
 
   @tailrec
   private def reduce(root: Number): Number = {
-    val exploded = root.explode
+    val exploded = explode(root)
     if (exploded.equals(root)) {
       val splitted = root.split
       if (splitted.equals(root)) {
@@ -145,7 +133,7 @@ class Day18 extends Puzzle with RegexParsers {
   }
 
   override def solvePart1(lines: List[String]): Long = {
-    val sum = reduce(lines.map(l => reduce(parseSnailfishNumber(l))).reduce((a, b) => {
+    val sum = reduce(lines.map(l => reduce(parse(l))).reduce((a, b) => {
       reduce(Node(a, b))
     }))
     sum.magnitude
