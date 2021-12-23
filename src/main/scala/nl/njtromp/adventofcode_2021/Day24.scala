572157103962d6c2f4527312029daf858b0fc2ba
Nico Tromp <nico.tromp@gmail.com>
2021-12-24 00:28:50 +0100

    Prepare for day 24.

---
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day24.scala
@@ -0,0 +1,14 @@
+package nl.njtromp.adventofcode_2021
+
+import nl.njtromp.adventofcode.Puzzle
+
+class Day24 extends Puzzle {
+
+  override def solvePart1(lines: List[String]): Long = ???
+
+  override def solvePart2(lines: List[String]): Long = ???
+}
+
+object Day24 extends App {
+  new Day24().solvePuzzles("/2021/day24.txt")
+}
---
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day24Test.scala
@@ -0,0 +1,20 @@
+package nl.njtromp.adventofcode_2021
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.io.Source
+
+class Day24Test extends AnyFlatSpec {
+  val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day24-example.txt")).getLines().toList
+
+  "Part 1" should "solve example correct" in {
+    assert(new Day24().solvePart1(example) === 0)
+  }
+
+  "Part 2" should "solve example correct" in {
+    assert(new Day24().solvePart2(example) === 0)
+  }
+
+
+
+}
