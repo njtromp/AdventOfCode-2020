4ab0abfe4d519a98e0fc70990fee52704d822b2b
Nico Tromp <nico.tromp@gmail.com>
2022-01-01 13:14:07 +0100

    Tests for part 1 & 2 for day 22 pass with new implementation.
    The new implementation is slower for part 1 but can handle part 2 without any OutOfMemory errors.

--- src/main/scala/nl/njtromp/adventofcode_2021/Day22.scala
+++ src/main/scala/nl/njtromp/adventofcode_2021/Day22.scala
@@ -7,15 +7,15 @@ import scala.annotation.tailrec
 class Day22 extends Puzzle {
 
   private val CubesOnOff = raw"(.+) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)".r
-  private val InitRange = (-50 to 50)
+  private val InitRange = -50 to 50
   private val InitCube = Cube(InitRange, InitRange, InitRange)
 
   case class Cube(x: Range, y: Range, z: Range) {
     private def overlapping(a: Range, b: Range): Range = {
       val overlap = a.intersect(b)
-      (overlap.min to overlap.max)
+      overlap.min to overlap.max
     }
-    def size: BigInt = x.size.toLong * y.size.toLong * z.size.toLong
+    def size: Long = x.size.toLong * y.size.toLong * z.size.toLong
     def points: Set[(Int, Int, Int)] = x.flatMap(x => y.flatMap(y => z.map((x, y, _)))).toSet
     def overlap(c : Cube): Boolean = {
       x.intersect(c.x).nonEmpty &&
@@ -27,7 +27,7 @@ class Day22 extends Puzzle {
         y.min <= c.y.min && y.max >= c.y.max &&
         z.min <= c.z.min && z.max >= c.z.max
     }
-    def overlapping(c: Cube): Cube = Cube(overlapping(x, c.x), overlapping(y, c.y), overlapping(z, c.z))
+    def intersect(c: Cube): Cube = Cube(overlapping(x, c.x), overlapping(y, c.y), overlapping(z, c.z))
   }
   case class Instruction(ins: String, cube: Cube)
 
@@ -43,43 +43,49 @@ class Day22 extends Puzzle {
     })
   }
 
-  def startReactor(instructions: List[Instruction]): BigInt = {
-    @tailrec
-    def startReactor(activeCubes: BigInt, processedInstruction: List[Instruction], instructions: List[Instruction]): BigInt = {
-      if (instructions.isEmpty)
-        activeCubes
-      else {
-        val instruction = instructions.head
-        val cube = instruction.cube
-        val activatedCubes = instruction.ins match {
-          case "on" => instruction.cube.size - processedInstruction.filterNot(_.cube.overlap(cube)).map(o =>
-            if (o.cube.overlap(cube))
-              o.cube.overlapping(cube).size
-            else
-              BigInt(0)
-            ).sum
-          case "off" => BigInt(0) - processedInstruction.map(o =>
-            if (o.cube.overlap(cube))
-              o.cube.overlapping(cube).size
-            else
-              BigInt(0)
-          ).sum
-        }
-        startReactor(activeCubes + activatedCubes, processedInstruction ++ List(instruction), instructions.tail)
+  def parts(outer: Range, inner: Range): List[Range] =
+//    List(outer.min to inner.min - 1, inner.min to inner.max, inner.max + 1 to outer.max).filterNot(_.isEmpty)
+    List(outer.min until inner.min, inner.min to inner.max, inner.max + 1 to outer.max).filterNot(_.isEmpty)
+
+  private var instructionCounter = 1
+  def performInstruction(onCubes: List[Cube], instruction: Instruction): List[Cube]= {
+    println(s"Instruction $instructionCounter")
+    instructionCounter += 1
+    val newCube = instruction.cube
+    val noneOverlapping = onCubes.filterNot(newCube.overlap)
+    val overlapping = onCubes.filter(newCube.overlap).flatMap(c => {
+      if (newCube.contains(c)) {
+        List.empty // The contained cube will either be removed (off) of replaced (on) so it will always be removed
+      } else {
+        val intersection = newCube.intersect(c)
+        // Chop along each axis into at most 3 ranges and combine that into at most 27 partial cubes ...
+        // ... and remove the overlapping (intersection) partial cube.
+        // If the instruction is a 'on' instruction the intersected part will be included by newCube
+        val xParts = parts(c.x, intersection.x)
+        val yParts = parts(c.y, intersection.y)
+        val zParts = parts(c.z, intersection.z)
+        xParts.flatMap(x => yParts.flatMap(y => zParts.map(z => Cube(x, y, z)))).filterNot(_ == intersection)
       }
+    })
+    if (instruction.ins == "on") {
+      newCube :: overlapping ++ noneOverlapping
+    } else {
+      overlapping ++ noneOverlapping
     }
-    startReactor(0, List.empty, instructions)
+  }
+
+  def startReactor(instructions: List[Instruction]): List[Cube] = {
+    instructions.foldLeft(List.empty[Cube])(performInstruction)
   }
 
   override def solvePart1(lines: List[String]): Long = {
     val instructions = readInstructions(lines).filter(_.cube.overlap(InitCube))
-//    590784
-    startReactor(instructions).toLong
+    startReactor(instructions).map(_.size).sum
   }
 
   override def solvePart2(lines: List[String]): Long = {
     val instructions = readInstructions(lines)
-    startReactor(instructions).toLong
+    startReactor(instructions).map(_.size).sum
   }
 }
 
--- src/test/scala/nl/njtromp/adventofcode_2021/Day22Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2021/Day22Test.scala
@@ -5,14 +5,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 import scala.io.Source
 
 class Day22Test extends AnyFlatSpec {
-  val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day22-example.txt")).getLines().toList
+  val example_1: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day22-example-1.txt")).getLines().toList
+  val example_2: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2021/day22-example-2.txt")).getLines().toList
 
   "Part 1" should "solve example correct" in {
-    assert(new Day22().solvePart1(example) === 590784)
+    assert(new Day22().solvePart1(example_1) === 590784)
   }
 
   "Part 2" should "solve example correct" in {
-    assert(new Day22().solvePart2(example) === 2758514936282235L)
+    assert(new Day22().solvePart2(example_2) === 2758514936282235L)
   }
 
 
