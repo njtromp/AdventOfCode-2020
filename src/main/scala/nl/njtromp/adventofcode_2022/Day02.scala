340a75537f44f63aeefb88df02906aaa9d3d41fe
Nico Tromp <nico.tromp@gmail.com>
2022-12-01 10:56:57 +0100

    Solve day 1 of 2022 and prepare for day 2.

---
+++ src/main/scala/nl/njtromp/adventofcode_2022/Day02.scala
@@ -0,0 +1,13 @@
+package nl.njtromp.adventofcode_2022
+
+import nl.njtromp.adventofcode.Puzzle
+
+class Day02 extends Puzzle {
+  override def solvePart1(lines: List[String]): Long = ???
+
+  override def solvePart2(lines: List[String]): Long = ???
+}
+
+object Day02 extends App{
+  new Day02().solvePuzzles("/2022/day02.txt")
+}
