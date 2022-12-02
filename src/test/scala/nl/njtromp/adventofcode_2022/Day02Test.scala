ee686f5d7d81cdbd3f907438b92787becad37a70
Nico Tromp <nico.tromp@gmail.com>
2022-12-02 06:32:21 +0100

    Scala is a little rusty, day 2.

--- src/test/scala/nl/njtromp/adventofcode_2022/Day02Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2022/Day02Test.scala
@@ -8,11 +8,11 @@ class Day02Test extends AnyFlatSpec {
   val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2022/day02-example.txt")).getLines().toList
 
   "Part 1" should "solve example correct" in {
-    assert(new Day02().solvePart1(example) === 0)
+    assert(new Day02().solvePart1(example) === 15)
   }
 
   "Part 2" should "solve example correct" in {
-    assert(new Day02().solvePart2(example) === 0)
+    assert(new Day02().solvePart2(example) === 12)
   }
 
 }
