38e526807c6e202fd3d51ace9c823a060cd32c98
Nico Tromp <nico.tromp@gmail.com>
2022-12-03 08:15:36 +0100

    Prepare for day 4.

---
+++ src/main/scala/nl/njtromp/adventofcode_2022/Day04.scala
@@ -0,0 +1,13 @@
+package nl.njtromp.adventofcode_2022
+
+import nl.njtromp.adventofcode.Puzzle
+
+class Day04 extends Puzzle {
+  override def solvePart1(lines: List[String]): Long = -1
+
+  override def solvePart2(lines: List[String]): Long = -1
+}
+
+object Day04 extends App{
+  new Day04().solvePuzzles("/2022/day04.txt")
+}
--- src/test/scala/nl/njtromp/adventofcode_2022/Day03Test.scala
+++ src/test/scala/nl/njtromp/adventofcode_2022/Day03Test.scala
@@ -8,11 +8,11 @@ class Day03Test extends AnyFlatSpec {
   val example: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/2022/day03-example.txt")).getLines().toList
 
   "Part 1" should "solve example correct" in {
-    assert(new Day03().solvePart1(example) === 0)
+    assert(new Day03().solvePart1(example) === 157)
   }
 
   "Part 2" should "solve example correct" in {
-    assert(new Day03().solvePart2(example) === 0)
+    assert(new Day03().solvePart2(example) === 70)
   }
 
 }
