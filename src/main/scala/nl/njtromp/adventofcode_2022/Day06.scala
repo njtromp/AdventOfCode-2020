53e6b9792b596da2120c3262aca87678c1419060
Nico Tromp <nico.tromp@gmail.com>
2022-12-06 06:35:59 +0100

    Day 06.

--- src/main/scala/nl/njtromp/adventofcode_2022/Day06.scala
+++ src/main/scala/nl/njtromp/adventofcode_2022/Day06.scala
@@ -4,16 +4,22 @@ import nl.njtromp.adventofcode.Puzzle2
 
 class Day06 extends Puzzle2 {
 
-  override def exampleAnswerPart1: Long = 0
+  override def exampleAnswerPart1: Long = 7
+
+  def isMarker(s: String, size: Int): Boolean = s.toSet.size == size
 
   override def solvePart1(lines: List[String]): Long = {
-    -1
+    lines.head.sliding(4, 1).foldLeft(-4)((a, s) => if (a > 0) a else {
+      if (isMarker(s, 4)) -a else a - 1
+    })
   }
 
-  override def exampleAnswerPart2: Long = 0
+  override def exampleAnswerPart2: Long = 19
 
   override def solvePart2(lines: List[String]): Long = {
-    -1
+    lines.head.sliding(14, 1).foldLeft(-14)((a, s) => if (a > 0) a else {
+      if (isMarker(s, 14)) -a else a - 1
+    })
   }
 
 }
