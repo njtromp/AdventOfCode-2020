7e5e4e7720025fbc7ae09d1e2010e382fd7c9230
Nico Tromp <nico.tromp@gmail.com>
2022-12-09 09:43:48 +0100

    Day 09 part 1 reduced.

--- src/main/scala/nl/njtromp/adventofcode_2022/Day09.scala
+++ src/main/scala/nl/njtromp/adventofcode_2022/Day09.scala
@@ -4,9 +4,19 @@ import nl.njtromp.adventofcode.{Puzzle2, SimpleMapTypes}
 
 class Day09 extends Puzzle2 with SimpleMapTypes{
 
-  abstract class Move(var n: Int) {
-    def doMove(head: Pos, tail: Pos): (Pos, Pos)
-    def moveTail(head: Pos, tail: Pos): Pos
+  class Move(delta: Pos, var n: Int) {
+    def doMove(head: Pos, tail: Pos): (Pos, Pos) =
+      ((head._1 + delta._1, head._2 + delta._2), moveTail((head._1 +delta._1, head._2 + delta._2), tail))
+
+    def moveTail(head: Pos, tail: Pos): Pos =
+      if (Math.abs(tail._1 - head._1) > 1 || Math.abs(tail._2 - head._2) > 1) {
+        if (delta._1 == 0)
+          (tail._1 + (head._1 - tail._1), tail._2 + delta._2)
+        else
+          (tail._1 + delta._1, tail._2 + (head._2 - tail._2))
+      } else
+        tail
+
     def moveRope(head: Pos, tail: Pos): (Pos, Pos, List[Pos]) = {
       var newHead = head
       var newTail = tail
@@ -23,59 +33,22 @@ class Day09 extends Puzzle2 with SimpleMapTypes{
       (newHead, newTail, positions)
     }
   }
-  class Up(n: Int) extends Move(n) {
-    override def doMove(head: Pos, tail: Pos): (Pos, Pos) =
-      ((head._1, head._2 + 1), moveTail((head._1, head._2 + 1), tail))
-    def moveTail(head: Pos, tail: Pos): Pos =
-      if (Math.abs(tail._1 - head._1) > 1 || Math.abs(tail._2 - head._2) > 1) {
-        (tail._1 + (head._1 - tail._1), tail._2 + 1)
-      } else
-        tail
-  }
-  class Down(n: Int) extends Move(n) {
-    override def doMove(head: Pos, tail: Pos): (Pos, Pos) =
-      ((head._1, head._2 - 1), moveTail((head._1, head._2 - 1), tail))
-    def moveTail(head: Pos, tail: Pos): Pos =
-      if (Math.abs(tail._1 - head._1) > 1 || Math.abs(tail._2 - head._2) > 1) {
-        (tail._1 + (head._1 - tail._1), tail._2 - 1)
-      } else
-        tail
-  }
-  class Right(n: Int) extends Move(n) {
-    override def doMove(head: Pos, tail: Pos): (Pos, Pos) =
-      ((head._1 + 1, head._2), moveTail((head._1 + 1, head._2), tail))
-    def moveTail(head: Pos, tail: Pos): Pos =
-      if (Math.abs(tail._1 - head._1) > 1 || Math.abs(tail._2 - head._2) > 1) {
-        (tail._1 + 1, tail._2 + (head._2 - tail._2))
-      } else
-        tail
-  }
-  class Left(n: Int) extends Move(n) {
-    override def doMove(head: Pos, tail: Pos): (Pos, Pos) =
-      ((head._1 - 1, head._2), moveTail((head._1 - 1, head._2), tail))
-    def moveTail(head: Pos, tail: Pos): Pos =
-      if (Math.abs(tail._1 - head._1) > 1 || Math.abs(tail._2 - head._2) > 1) {
-        (tail._1 - 1, tail._2 + (head._2 - tail._2))
-      } else
-        tail
-  }
 
   def decodeMove(line: String): Move =
     line.head match {
-      case 'U' => (new Up(line.substring(2).toInt))
-      case 'D' => (new Down(line.substring(2).toInt))
-      case 'L' => (new Left(line.substring(2).toInt))
-      case 'R' => (new Right(line.substring(2).toInt))
+      case 'U' => new Move(up, line.substring(2).toInt)
+      case 'D' => new Move(down, line.substring(2).toInt)
+      case 'L' => new Move(left, line.substring(2).toInt)
+      case 'R' => new Move(right, line.substring(2).toInt)
     }
 
   override def exampleAnswerPart1: Long = 13
 
   def moveRope(lines: List[String], head: Pos, tail: Pos): List[Pos] = {
     lines match {
-      case line :: remaining => {
+      case line :: remaining =>
         val newPos = decodeMove(line).moveRope(head, tail)
         newPos._3 ++ moveRope(remaining, newPos._1, newPos._2)
-      }
       case Nil => Nil
     }
   }
@@ -86,7 +59,7 @@ class Day09 extends Puzzle2 with SimpleMapTypes{
     val minY = positions.map(_._2).min
     val maxY = positions.map(_._2).max
     for (y <- Range(maxY, minY, -1).inclusive) {
-      for (x <- (minX to maxX)) {
+      for (x <- minX to maxX) {
         print(if (positions.contains((x, y))) '#' else '.')
       }
       println
@@ -99,8 +72,6 @@ class Day09 extends Puzzle2 with SimpleMapTypes{
 //    printPositions(tailPositions)
     tailPositions.distinct.size
   }
-
-  // 6340 te hoog
 
   override def exampleAnswerPart2: Long = 36
 
