cc795c1df607378b86c3ce170703f19c42ec0b43
Nico Tromp <nico.tromp@gmail.com>
2023-01-28 17:04:39 +0100

    Papare.

---
+++ src/main/scala/nl/njtromp/adventofcode_2018/Day00.scala
@@ -0,0 +1,23 @@
+package nl.njtromp.adventofcode_2018
+
+import nl.njtromp.adventofcode.Puzzle2
+
+import scala.annotation.tailrec
+
+class Day00 extends Puzzle2 {
+
+  override def exampleAnswerPart1: Long = 0
+  override def solvePart1(lines: List[String]): Long = {
+    -1
+  }
+
+  override def exampleAnswerPart2: Long = 0
+  override def solvePart2(lines: List[String]): Long = {
+    -1
+  }
+
+}
+
+object Day00 extends App {
+  new Day00().solvePuzzles("/2018/day00.txt")
+}
