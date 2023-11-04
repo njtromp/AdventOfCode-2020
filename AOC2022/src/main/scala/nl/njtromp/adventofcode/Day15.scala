678c69f1d35efe51625f649f258f67d14ccecf05
Nico Tromp <nico.tromp@gmail.com>
2023-11-04 19:58:52 +0100

    Solved day 15.

--- AOC2022/src/main/scala/nl/njtromp/adventofcode/Day15.scala
+++ AOC2022/src/main/scala/nl/njtromp/adventofcode/Day15.scala
@@ -2,7 +2,9 @@ package nl.njtromp.adventofcode
 
 class Day15 extends Puzzle[Long] {
   type Pos = (Long, Long)
+
   private val sensorReading = "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)".r
+
   private case class SensorReading(sX: Long, sY: Long, bX: Long, bY: Long) {
     val range: Long = distance(sX - bX, sY - bY)
     def distance(deltaX: Long, deltaY: Long): Long = Math.abs(deltaX) + Math.abs(deltaY)
@@ -14,15 +16,15 @@ class Day15 extends Puzzle[Long] {
         None
   }
 
-  private def combine(ranges: List[LongRange]): List[LongRange] = ranges match {
+  private def combineRanges(ranges: List[LongRange]): List[LongRange] = ranges match {
     case Nil => Nil
     case r :: Nil => r :: Nil
     case a :: b :: tail =>
       val combined = a.combine(b)
       if (combined.size == 1)
-        combine(combined.head :: tail)
+        combineRanges(combined.head :: tail)
       else
-        a :: combine(b :: tail)
+        a :: combineRanges(b :: tail)
   }
 
   private var y = 10L
@@ -30,15 +32,20 @@ class Day15 extends Puzzle[Long] {
   override def solvePart1(lines: List[String]): Long =
     val readings = lines.map({case sensorReading(sX, sY, bX, bY) => SensorReading(sX.toLong, sY.toLong, bX.toLong, bY.toLong)})
     val beacons  = readings.filter(_.bY == y).map(_.bY).toSet
-    val sortedRanges = combine(readings.map(_.rangeAt(y)).filter(_.nonEmpty).map(_.get).sortBy(_.start))
+    val sortedRanges = combineRanges(readings.map(_.rangeAt(y)).filter(_.nonEmpty).map(_.get).sortBy(_.start))
     y = 2000000L
     sortedRanges.map(_.size).sum - beacons.size
 
-  private var max = 20
+  private var max = 20L
   override def exampleAnswerPart2: Long = 56000011
   override def solvePart2(lines: List[String]): Long =
-//    max = 4000000
-    -1
+    val readings = lines.map({ case sensorReading(sX, sY, bX, bY) => SensorReading(sX.toLong, sY.toLong, bX.toLong, bY.toLong) })
+    val minY = readings.minBy(_.sY).sY
+    val maxY = readings.maxBy(_.sY).sY
+    val y = (minY to maxY).filter(y => combineRanges(readings.map(_.rangeAt(y)).filter(_.nonEmpty).map(_.get).sortBy(_.start)).size == 2).head
+    val gapped = combineRanges(readings.map(_.rangeAt(y)).filter(_.nonEmpty).map(_.get).sortBy(_.start))
+    max = 4000000L
+    (gapped.head.last + 1) * max + y
 }
 
 object Day15 extends App{
