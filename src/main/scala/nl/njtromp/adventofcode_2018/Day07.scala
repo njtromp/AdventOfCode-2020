7b18b0ddf508c8c85416e652380be855e1995e58
Nico Tromp <nico.tromp@gmail.com>
2023-11-14 13:39:02 +0100

    Part 1 of day 7 of 2018 solved.

--- src/main/scala/nl/njtromp/adventofcode_2018/Day07.scala
+++ src/main/scala/nl/njtromp/adventofcode_2018/Day07.scala
@@ -2,38 +2,46 @@ package nl.njtromp.adventofcode_2018
 
 import nl.njtromp.adventofcode.StringPuzzle
 
+import scala.annotation.tailrec
+import scala.collection.SortedSet
+
 class Day07 extends StringPuzzle {
-  private class Node(id: Char, var next: List[Node])
+  private val order = "Step (\\w) must be finished before step (\\w) can begin.".r
 
-  private def followPaths(nodes: List[Char], visited: Set[Char], finish: Char, next: Map[Char, List[Char]]): List[Char] = {
-    nodes.sorted match {
-      case Nil => List(finish)
-      case c :: tail => if (c == finish) List(c) else c :: followPaths(tail ++ next(c).filterNot(visited.contains), visited + c, finish, next)
+  private def performSteps(possibleSteps: SortedSet[String], stepOrder: Map[String, SortedSet[String]], requirements: Map[String, SortedSet[String]]): String = {
+    @tailrec
+    def step(possibleSteps: SortedSet[String], stepsPerformed: Set[String], order: String): String = {
+      if (possibleSteps.isEmpty)
+        order
+      else {
+        val stepToBePerformed = possibleSteps.head
+        val newStepsPerformed = stepsPerformed + stepToBePerformed
+        step(
+          possibleSteps.tail ++ stepOrder(stepToBePerformed).filter(s => !order.contains(s) && requirements(s).subsetOf(newStepsPerformed)),
+          newStepsPerformed,
+          order + stepToBePerformed
+        )
+      }
     }
-  }
 
-  private def removeDups(charaters: List[Char]): List[Char] = {
-    charaters match {
-      case Nil => Nil
-      case c :: tail => List(c) ++ removeDups(tail.dropWhile(_ == c))
-    }
+    step(possibleSteps, Set(possibleSteps.head), "")
   }
 
   override def exampleAnswerPart1: String = "CABDFE"
   override def solvePart1(lines: List[String]): String = {
-    val order = lines.map(l => (l.charAt(5), l.charAt(36)))
-    val next = order.groupMap(_._1)(_._2)
-    val starts = order.map(_._1).toSet
-    val finishes = order.map(_._2).toSet
-    val finish = finishes.diff(starts).head
-    starts.diff(finishes).toList.map(s => removeDups(followPaths(List(s), Set(finish), finish, next)).mkString).foreach(println)
-    // FDABEGHICKLMJNOPQRTUVWXYZS not good :-(
-    removeDups(followPaths(starts.diff(finishes).toList, Set(finish), finish, next)).mkString
-}
+    val emptyMapWithEmptySets = Map.empty[String, SortedSet[String]].withDefaultValue(SortedSet.empty[String])
+    val stepOrder = lines.foldLeft(emptyMapWithEmptySets)((stepOrder, step) => step match {
+      case order(parent, child) => stepOrder + (parent -> (stepOrder(parent) + child))
+    })
+    val requirements = lines.foldLeft(emptyMapWithEmptySets)((dependencies, step) => step match {
+      case order(parent, child) => dependencies + (child -> (dependencies(child) + parent))
+    })
+    performSteps(SortedSet.from(stepOrder.keySet diff stepOrder.flatMap(_._2).toSet), stepOrder, requirements)
+  }
 
-  override def exampleAnswerPart2: String = ""
+  override def exampleAnswerPart2: String = "nothing"
   override def solvePart2(lines: List[String]): String = {
-    "[Not implemented]"
+    "bla bla"
   }
 
 }
