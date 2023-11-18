8cadd95afc82892d4fb1e023f497897fdc8ff451
Nico Tromp <nico.tromp@gmail.com>
2023-11-18 10:23:13 +0100

    Add BFS to RouteFinding.

--- AOC2022/src/main/scala/nl/njtromp/adventofcode/RouteFinding.scala
+++ AOC2022/src/main/scala/nl/njtromp/adventofcode/RouteFinding.scala
@@ -7,24 +7,52 @@ import nl.njtromp.adventofcode.{SimpleMap, SimpleMapTypes}
 
 trait RouteFinding extends SimpleMapTypes {
 
+  def bfs[A](start: A, finish: A, neighbors: A => List[A]): List[A] =
+    val source = mutable.Map.empty[A, A]
+    val toBeVisited = mutable.Queue.empty[A]
+    val visited = mutable.Set.empty[A]
+    toBeVisited.enqueue(start)
+    while (toBeVisited.nonEmpty)
+      val current = toBeVisited.dequeue()
+      if (current == finish)
+        return reconstructPath(start, finish, source)
+      if (!visited.contains(current))
+        visited += current
+        neighbors(current).foreach(n => {
+          if (!visited.contains(n))
+            toBeVisited.enqueue(n)
+        })
+    Nil
+
+  def bfs[A](map: SimpleMap[A], canReach: (A, A) => Boolean, start: Pos, finish: Pos): List[Pos] =
+    val source = mutable.Map.empty[Pos, Pos]
+    val toBeVisited = mutable.Queue.empty[Pos]
+    val visited = mutable.Set.empty[Pos]
+    toBeVisited.enqueue(start)
+    while (toBeVisited.nonEmpty)
+      val current = toBeVisited.dequeue()
+      if (current == finish)
+        return reconstructPath(start, finish, source)
+      if (!visited.contains(current))
+        visited += current
+        val neighbors = map.neighborPositions(current, square).filter(n => canReach(map(current), map(n)))
+        neighbors.foreach(n => {
+          if (!visited.contains(n))
+            toBeVisited.enqueue(n)
+        })
+    Nil
+
   def dijkstra[A](start: A, finish: A, neighbors: A => List[A], weight: (A, A) => Long): List[A] =
     val distanceToStart = mutable.Map.empty[A, Long].withDefaultValue(Long.MaxValue)
     val source = mutable.Map.empty[A, A]
     val toBeVisited = mutable.PriorityQueue.empty[A](Ordering.by(n => -distanceToStart(n)))
     val visited = mutable.Set.empty[A]
-
-    def constructPath(current: A): List[A] =
-      if (current == start)
-        List(start)
-      else
-        constructPath(source(current)) ++ List(current)
-
     distanceToStart += start -> 0
     toBeVisited.enqueue(start)
     while (toBeVisited.nonEmpty)
       val current = toBeVisited.dequeue()
       if (current == finish)
-        return constructPath(finish)
+        return reconstructPath(start, finish, source)
       // The queue might hold positions with an 'old' (lower) priority so if we encounter one of them, we must skip it.
       if (!visited.contains(current))
         visited += current
@@ -45,19 +73,12 @@ trait RouteFinding extends SimpleMapTypes {
     val source = mutable.Map.empty[Pos, Pos]
     val toBeVisited = mutable.PriorityQueue.empty[Pos](Ordering.by(p => -distanceToStart(p)))
     val visited = mutable.Set.empty[Pos]
-
-    def constructPath(current: Pos): List[Pos] =
-      if (current == start)
-        List(start)
-      else
-        constructPath(source(current)) ++ List(current)
-
     distanceToStart += start -> 0
     toBeVisited.enqueue(start)
     while (toBeVisited.nonEmpty)
       val current = toBeVisited.dequeue()
       if (current == finish)
-        return constructPath(finish)
+        return reconstructPath(start, finish, source)
       // The queue might hold positions with an 'old' (lower) priority so if we encounter one of them, we must skip it.
       if (!visited.contains(current))
         visited += current
@@ -72,6 +93,14 @@ trait RouteFinding extends SimpleMapTypes {
             source += n -> current
         })
     Nil
+
+  private def reconstructPath[A](start: A, current: A, source: mutable.Map[A, A]): List[A] =
+    def reconstructPath(current: A): List[A] =
+    if (current == start)
+      List(start)
+    else
+      reconstructPath(source(current)) ++ List(current)
+    reconstructPath(current)
 
   def printColoredPath[A](map: SimpleMap[A], path: List[Pos], colorMapping: A => Int): Unit =
     println
