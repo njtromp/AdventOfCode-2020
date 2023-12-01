a2cb3d8d3b1b9452adb48ea596c111067917a6e7
Nico Tromp <nico.tromp@gmail.com>
2023-12-01 19:58:38 +0100

    Nicer solutions for day 01.

---
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day01B.scala
@@ -0,0 +1,39 @@
+package nl.njtromp.adventofcode
+
+import nl.njtromp.adventofcode.Day01.{isExample1, isExample2}
+
+class Day01B extends Puzzle[Long] {
+  private val digits = List("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+  override def exampleAnswerPart1: Long = 142
+  override def solvePart1(lines: List[String]): Long =
+    val actualLines = if isExample1 then lines.take(4) else lines
+    isExample1 = false
+    actualLines.map(_.filter(_.isDigit)).map(l => l.head.asDigit * 10 + l.last.asDigit).sum
+
+  override def exampleAnswerPart2: Long = 281
+  override def solvePart2(lines: List[String]): Long =
+    val actualLines = if isExample2 then lines.drop(4) else lines
+    isExample2 = false
+    def toDigits(line: String): String =
+      if line.isBlank then
+        line
+      else if line.head.isDigit then
+        line.head + toDigits(line.tail)
+      else {
+        val decodedDigits = digits.filter(line.indexOf(_) == 0).map(d => digits.indexOf(d))
+        if decodedDigits.isEmpty then
+          toDigits(line.tail)
+        else
+          decodedDigits.head.toString.head + toDigits(line.tail)
+      }
+
+    actualLines.map(toDigits).map(l => l.head.asDigit * 10 + l.last.asDigit).sum
+
+}
+
+object Day01B extends App{
+  var isExample1 = true
+  var isExample2 = true
+  new Day01B().solvePuzzles("/day01.txt")
+}
---
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day01C.scala
@@ -0,0 +1,36 @@
+package nl.njtromp.adventofcode
+
+import nl.njtromp.adventofcode.Day01.{isExample1, isExample2}
+
+class Day01C extends Puzzle[Long] {
+  private val digitMapping = Map(
+    "one" ->  "1",
+    "two" -> "2",
+    "three" -> "3",
+    "four" ->"4",
+    "five" -> "5",
+    "six" -> "6",
+    "seven" -> "7",
+    "eight" -> "8",
+    "nine" -> "9"
+  )
+
+  override def exampleAnswerPart1: Long = 142
+  override def solvePart1(lines: List[String]): Long =
+    val actualLines = if isExample1 then lines.take(4) else lines
+    isExample1 = false
+    actualLines.map(_.filter(_.isDigit)).map(l => l.head.asDigit * 10 + l.last.asDigit).sum
+
+  override def exampleAnswerPart2: Long = 281
+  override def solvePart2(lines: List[String]): Long =
+    val actualLines = if isExample2 then lines.drop(4) else lines
+    isExample2 = false
+    solvePart1(actualLines.map(l => digitMapping.foldLeft(l)((l, d) => l.replaceAll(d._1, d._2))))
+
+}
+
+object Day01C extends App {
+  var isExample1 = true
+  var isExample2 = true
+  new Day01C().solvePuzzles("/day01.txt")
+}
