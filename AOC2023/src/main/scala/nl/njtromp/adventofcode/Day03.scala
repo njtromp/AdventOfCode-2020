07ff2dab0a2b42d3ed85b193f2011106846751b7
Nico Tromp <nico.tromp@gmail.com>
2023-12-02 19:17:34 +0100

    Prepare for day  03.

---
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day03.scala
@@ -0,0 +1,17 @@
+package nl.njtromp.adventofcode
+
+class Day03 extends Puzzle[Long] {
+
+  override def exampleAnswerPart1: Long = 0
+  override def solvePart1(lines: List[String]): Long =
+    -1
+
+  override def exampleAnswerPart2: Long = 0
+  override def solvePart2(lines: List[String]): Long =
+    -1
+
+}
+
+object Day03 extends App {
+  new Day03().solvePuzzles("/day03.txt")
+}
