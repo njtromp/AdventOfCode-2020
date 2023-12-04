0c68d7d6906252351918e576b4dd0ea77b7d29d4
Nico Tromp <nico.tromp@gmail.com>
2023-12-04 08:11:47 +0100

    Day 02 with parser-combinator :-).

--- AOC2023/src/main/scala/nl/njtromp/adventofcode/Day02B.scala
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day02B.scala
@@ -13,20 +13,20 @@ class Day02B extends Puzzle[Long] with RegexParsers {
     def maxBalls(color: String): Int = sets.map(_(color)).max
   }
 
-  def number: Parser[Int] = """\d+""".r ^^ {_.toInt}
-  def color: Parser[String] = s"""$red|$blue|$green""".r ^^ { color => color}
-  def ball: Parser[Ball] = number ~ color ^^ {case count ~ color => Ball(color, count)}
+  def number: Parser[Int] = """\d+""".r ^^ { _.toInt }
+  def color: Parser[String] = s"""$red|$blue|$green""".r ^^ { color => color }
+  def ball: Parser[Ball] = number ~ color ^^ { case count ~ color => Ball(color, count) }
   def balls: Parser[List[Ball]] = repsep(ball, ",")
   def game: Parser[Game] = "Game" ~ number ~ ":" ~ repsep(balls, ";") ^^ { case "Game" ~ id ~ ":" ~ takes => Game(id, takes) }
 
   override def exampleAnswerPart1: Long = 8
   override def solvePart1(lines: List[String]): Long =
-    val games = lines.map(l => parseAll[Game](game, l) match { case Success(game, _) => game })
+    val games = lines.map(l => parseAll(game, l) match { case Success(game, _) => game })
     games.filter(g => g.maxBalls(red) <= 12 && g.maxBalls(green) <= 13 && g.maxBalls(blue) <= 14).map(_.id).sum
 
   override def exampleAnswerPart2: Long = 2286
   override def solvePart2(lines: List[String]): Long =
-    val games = lines.map(l => parseAll[Game](game, l) match { case Success(game, _) => game })
+    val games = lines.map(l => parseAll(game, l) match { case Success(game, _) => game })
     games.map(g => g.maxBalls(red) * g.maxBalls(green) * g.maxBalls(blue)).sum
 }
 
