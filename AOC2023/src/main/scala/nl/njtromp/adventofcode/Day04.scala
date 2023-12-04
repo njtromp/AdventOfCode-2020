138fe02da55c03ce46ce5573095af68e849af147
Nico Tromp <nico.tromp@gmail.com>
2023-12-04 10:41:02 +0100

    Use parser-combinator for cleaner parsing of input.

--- AOC2023/src/main/scala/nl/njtromp/adventofcode/Day04.scala
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day04.scala
@@ -1,20 +1,23 @@
 package nl.njtromp.adventofcode
 
-class Day04 extends Puzzle[Long] {
-  private case class ScratchCard(id: Int, winningNumbers: Set[Int], numbers: Set[Int]) {
-    private def numberOfMatchingCards(): Int = (winningNumbers intersect numbers).size
-    def score(): Int = Math.pow(2, numberOfMatchingCards() - 1).toInt // 0.5 => 0, :-)
+import scala.util.parsing.combinator.RegexParsers
+
+class Day04 extends Puzzle[Long] with RegexParsers {
+  case class ScratchCard(id: Int, winningNumbers: Set[Int], numbers: Set[Int]) {
+    def numberOfMatchingCards: Int = (winningNumbers intersect numbers).size
+    def score(): Int = Math.pow(2, numberOfMatchingCards - 1).toInt // 0.5 => 0, :-)
     def score(tail: Array[ScratchCard]): Int =
-      val copies = tail.take(numberOfMatchingCards())
+      val copies = tail.take(numberOfMatchingCards)
       1 + copies.indices.map(i => tail(i).score(tail.drop(i + 1))).sum
   }
 
+  def number: Parser[Int] = """\d+""".r ^^ { _.toInt }
+  def card: Parser[ScratchCard] = "Card" ~ number ~ ":" ~ rep(number) ~ "|" ~ rep(number) ^^ {
+    case  "Card" ~ id ~ ":" ~ winningNumbers ~ "|" ~ numbers => ScratchCard(id, winningNumbers.toSet, numbers.toSet)
+  }
+
   private def parseScratchCard(line: String): ScratchCard =
-    val parts = line.split('|')
-    val id = parts.head.split(':').head.split(' ').last.toInt
-    val winningNumbers = parts.head.split(':').last.split(' ').filter(_.nonEmpty).map(_.toInt).toSet
-    val numbers = parts.last.split(' ').filter(_.nonEmpty).map(_.toInt).toSet
-    ScratchCard(id, winningNumbers, numbers)
+    parse(card, line) match { case Success(card, _) => card}
 
   override def exampleAnswerPart1: Long = 13
   override def solvePart1(lines: List[String]): Long =
