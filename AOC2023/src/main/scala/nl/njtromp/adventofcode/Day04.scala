6000f6a5c144ed0bdc8bf21eaef331a80e285b0d
Nico Tromp <nico.tromp@gmail.com>
2023-12-04 12:09:44 +0100

    Even more speedup for part 2 :-)

--- AOC2023/src/main/scala/nl/njtromp/adventofcode/Day04.scala
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day04.scala
@@ -5,10 +5,10 @@ import scala.util.parsing.combinator.RegexParsers
 class Day04 extends Puzzle[Long] with RegexParsers {
   case class ScratchCard(id: Int, winningNumbers: Set[Int], numbers: Set[Int]) {
     lazy val numberOfMatchingCards: Int = (winningNumbers intersect numbers).size
-    def score(): Int = Math.pow(2, numberOfMatchingCards - 1).toInt // 0.5 => 0, :-)
-    def score(tail: Array[ScratchCard]): Int =
-      val copies = tail.take(numberOfMatchingCards)
-      1 + copies.indices.map(i => tail(i).score(tail.drop(i + 1))).sum
+    lazy val score: Int = Math.pow(2, numberOfMatchingCards - 1).toInt // 0.5 => 0, :-)
+    def score(cards: Array[ScratchCard]): Int =
+      val copies = cards.slice(id, id + numberOfMatchingCards)
+      1 + copies.map(c => c.score(cards)).sum
   }
 
   def number: Parser[Int] = """\d+""".r ^^ { _.toInt }
@@ -21,12 +21,12 @@ class Day04 extends Puzzle[Long] with RegexParsers {
 
   override def exampleAnswerPart1: Long = 13
   override def solvePart1(lines: List[String]): Long =
-    lines.map(parseScratchCard).map(_.score()).sum
+    lines.map(parseScratchCard).map(_.score).sum
 
   override def exampleAnswerPart2: Long = 30
   override def solvePart2(lines: List[String]): Long =
     val cards = lines.map(parseScratchCard).toArray
-    cards.indices.map(i => cards(i).score(cards.drop(i + 1))).sum
+    cards.map(c => c.score(cards)).sum
 }
 
 object Day04 extends App {
