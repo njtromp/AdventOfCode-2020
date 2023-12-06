111f4032b17bd36542a029155c5b0f6452c262f5
Nico Tromp <nico.tromp@gmail.com>
2023-12-06 08:25:52 +0100

    Solved part 1 and 2 of day 06.

--- AOC2023/src/main/scala/nl/njtromp/adventofcode/Day06.scala
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day06.scala
@@ -1,15 +1,48 @@
 package nl.njtromp.adventofcode
 
-class Day06 extends Puzzle[Long] {
+import scala.annotation.tailrec
+import scala.util.parsing.combinator.RegexParsers
 
-  override def exampleAnswerPart1: Long = 0
+class Day06 extends Puzzle[Long] with RegexParsers {
+  private def number: Parser[Long] = "\\d+".r ^^ { _.toLong }
+  private def numbers: Parser[List[Long]] = rep(number) ^^ { numbers => numbers}
+  private def parseNumbers(line: String): List[Long] = parse(numbers, line) match { case Success(numbers, _) => numbers }
+  private def calculateWinningTimes(time: Long, distance: Long): Long =
+    @tailrec
+    def findLimit(t: Long, delta: Long): Long =
+      if (time-t)*t > distance then
+        t
+      else
+        findLimit(t + delta, delta)
+    findLimit(time, -1) - findLimit(1, 1) + 1
+  // I was my preferred solution but did not recognized in time that I needed a correction for the last example
+  private def preferredCalculateWinningTimes(time: Long, distance: Long): Long =
+    // This correction is needed in case of equals distances
+    def correct(t: Long, correction: Long): Long =
+      if (time - t) * t == distance then t + correction else t
+    // distance < (time - t) * t
+    // distance < t*time - t^2
+    // 0 < -t^2 + time*t - distance
+    // t^2 -time*t + distance = 0
+    // a-b-c formula
+    val a = 1
+    val b = -time
+    val c = distance
+    val sqrt = Math.sqrt(b * b - 4 * a * c)
+    val range = LongRange(correct(Math.ceil((-b - sqrt) / 2 * a).toLong, 1), correct(Math.floor((-b + sqrt) / 2 * a).toLong, -1))
+    range.size
+
+  override def exampleAnswerPart1: Long = 288
   override def solvePart1(lines: List[String]): Long =
-    -1
+    val times = parseNumbers(lines.head.substring(10))
+    val distances = parseNumbers(lines.last.substring(10))
+    times.zip(distances).map(calculateWinningTimes).product
 
-  override def exampleAnswerPart2: Long = 0
+  override def exampleAnswerPart2: Long = 71503
   override def solvePart2(lines: List[String]): Long =
-    -1
-
+    val time = lines.head.substring(10).replaceAll(" ", "").toLong
+    val distance = lines.last.substring(10).replaceAll(" ", "").toLong
+    calculateWinningTimes(time, distance)
 }
 
 object Day06 extends App {
