b27aeedec1a01918d4ab1902163de52d835116c1
Nico Tromp <nico.tromp@gmail.com>
2023-12-09 12:24:50 +0100

    Solved part 1 and 2 of day 10.

---
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day10.scala
@@ -0,0 +1,17 @@
+package nl.njtromp.adventofcode
+
+class Day10 extends ParserPuzzle[Long] {
+
+  override def exampleAnswerPart1: Long = 0
+  override def solvePart1(lines: List[String]): Long =
+    -1
+
+  override def exampleAnswerPart2: Long = 0
+  override def solvePart2(lines: List[String]): Long =
+    -1
+
+}
+
+object Day10 extends App {
+  new Day10().solvePuzzles()
+}
