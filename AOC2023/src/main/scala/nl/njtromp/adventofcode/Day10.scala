15a4e4d82351fa2ca213bbbf131805b1cd289ce2
Nico Tromp <nico.tromp@gmail.com>
2023-12-10 09:19:22 +0100

    Solved part 1 and 2 of day 10.

--- AOC2023/src/main/scala/nl/njtromp/adventofcode/Day10.scala
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day10.scala
@@ -1,14 +1,101 @@
 package nl.njtromp.adventofcode
 
-class Day10 extends ParserPuzzle[Long] {
+class Day10 extends Puzzle[Long] with SimpleMapTypes {
+  private val westCandidates = List('-', 'F', 'L', 'S')
+  private val eastCandidates = List('-', 'J', '7', 'S')
+  private val northCandidates = List('|', 'F', '7', 'S')
+  private val southCandidates = List('|', 'J', 'L', 'S')
+  private val directions = Map(
+    (down, 'S') -> down,
+    (up, 'S') -> up,
+    (left, 'S') -> right,
+    (right, 'S') -> right,
+    (down, '|') -> down,
+    (up, '|') -> up,
+    (left, '-') -> left,
+    (right, '-') -> right,
+    (up, 'F') -> right,
+    (left, 'F') -> down,
+    (up, '7') -> left,
+    (right, '7') -> down,
+    (down, 'J') -> left,
+    (right, 'J') -> up,
+    (down, 'L') -> right,
+    (left, 'L') -> up,
+  )
+  private val candidates = Map(up -> northCandidates, down -> southCandidates, left -> westCandidates, right -> eastCandidates)
 
-  override def exampleAnswerPart1: Long = 0
+  private val extendHorizontal = Map(
+    '-' -> '-',
+    'F' -> '-',
+    'L' -> '-'
+  ).withDefault(_ => '.')
+  private val extendVertical = Map(
+    '|' -> '|',
+    'F' -> '|',
+    '7' -> '|'
+  ).withDefault(_ => '.')
+
+  private def isConnected(map: SimpleMap[Char], current: Pos, direction: Delta, connections: List[Char]): Boolean =
+    val neighbors = map.neighborPositions(current, List(direction))
+    neighbors.exists(p => connections.contains(map(p)))
+
+  private def findLoop(map: SimpleMap[Char], start: Pos): List[Pos] =
+    def findLoop(current: Pos, move: Delta): List[Pos] = {
+      val nextPos = map.neighborPositions(current, List(move)).head
+      if map(nextPos) == 'S' then
+        List(current, nextPos)
+      else
+        val nextMove = directions((move, map(nextPos)))
+        current :: findLoop(nextPos, nextMove)
+    }
+    val startingDirection = square.filter(d => isConnected(map, start, d, candidates(d))).head
+    findLoop(start, startingDirection)
+
+  private def floodFill(map: SimpleMap[Char], start: Pos): Unit =
+    def floodFill(current: Pos): Unit =
+      if map(current).toUpper != 'X' then {
+        map(current) = 'x'
+        map.neighborPositions(current, square).filter(map(_).toUpper != 'X').map(floodFill)
+      }
+    floodFill(start)
+
+  override def exampleAnswerPart1: Long = 8
   override def solvePart1(lines: List[String]): Long =
-    -1
+    val actualLines = groupByEmptyLine(lines).head
+    val map = SimpleMap[Char](actualLines, _.toCharArray)
+    val start = map.find('S').head
+    findLoop(map, start).size / 2
 
-  override def exampleAnswerPart2: Long = 0
+  override def exampleAnswerPart2: Long = 10
   override def solvePart2(lines: List[String]): Long =
-    -1
+    val actualLines = groupByEmptyLine(lines).last
+    // Extend vertical and horizontal.
+    // This ensures that there is a opening between adjacent pipes so floodfill can do its job properly.
+    val extendedMap = actualLines.map(_.flatMap(p => s"$p${extendHorizontal(p)}")).flatMap(l => List(l, l.map(extendVertical)))
+    val usableMap = extendedMap.dropRight(1).map(_.dropRight(1))
+    val map = SimpleMap[Char](usableMap, _.toCharArray)
+    val start = map.find('S').head
+    // This correction could be input specific, for me it did the trick :-)
+    map((start._1 + 1, start._2)) = '|'
+
+    // Mark the loop
+    val loop = findLoop(map, start)
+    loop.foreach(p => map(p) = 'X')
+
+    // Floodfill (mark with x, just for easier debugging) from the outside
+    (0 until map.height).foreach(r => {
+      floodFill(map, (r, 0))
+      floodFill(map, (r, map.width - 1))
+    })
+    (0 until map.width).foreach(c => {
+      floodFill(map, (0, c))
+      floodFill(map, (map.height - 1, c))
+    })
+
+    // Remove lines and columns needed for floodfill to work
+    val srinkedMap = (0 until map.height / 2).map(r => map.row(r * 2)).toList.map(r => r.zipWithIndex.filter(_._2 % 2 == 0).map(_._1).mkString(""))
+    srinkedMap.map(_.count(_.toUpper != 'X')).sum
 
 }
 
