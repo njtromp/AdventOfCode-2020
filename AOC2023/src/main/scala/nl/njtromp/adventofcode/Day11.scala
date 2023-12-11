7af522a3247f438be4d74939af37007ddf765318
Nico Tromp <nico.tromp@gmail.com>
2023-12-11 07:36:47 +0100

    Solved part 1 and 2 of day 11.

---
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day11.scala
@@ -0,0 +1,65 @@
+package nl.njtromp.adventofcode
+
+import scala.annotation.tailrec
+
+class Day11 extends Puzzle[Long] with SimpleMapTypes {
+
+  @tailrec
+  private def expandRows(starsOnRow: List[(Long, Long)], emptyRows: List[Long], extraRows: Long): List[(Long, Long)] =
+    if emptyRows.isEmpty then
+      starsOnRow
+    else
+      val beforeRow = starsOnRow.filter(_._1 < emptyRows.head)
+      val afterRow = starsOnRow.filter(_._1 > emptyRows.head)
+      expandRows(beforeRow ++ afterRow.map(s => (s._1 + extraRows, s._2)), emptyRows.tail.map(_ + extraRows), extraRows)
+
+  @tailrec
+  private def expandColumns(starsOnColumn: List[(Long, Long)], emptyColumns: List[Long], extraColumns: Long): List[(Long, Long)] =
+    if emptyColumns.isEmpty then
+      starsOnColumn
+    else
+      val beforeColumn = starsOnColumn.filter(_._2 < emptyColumns.head)
+      val afterColumn = starsOnColumn.filter(_._2 > emptyColumns.head)
+      expandColumns(beforeColumn ++ afterColumn.map(s => (s._1, s._2 + extraColumns)), emptyColumns.tail.map(_ + extraColumns), extraColumns)
+
+  private def distance(a: (Long, Long), b: (Long, Long)): Long =
+    Math.abs(a._1 - b._1) + Math.abs(a._2 - b._2)
+
+  override def exampleAnswerPart1: Long = 374
+  override def solvePart1(lines: List[String]): Long =
+    val stars = lines.zipWithIndex.flatMap(r => r._1.toCharArray.zipWithIndex.filter(_._1 == '#').map(s => (r._2.toLong, s._2.toLong)))
+    val starsOnRow = stars.groupBy(_._1)
+    val minRow = stars.map(_._1).min
+    val maxRow = stars.map(_._1).max
+    val emptyRows = ((minRow to maxRow).toSet diff starsOnRow.keySet).toList.sorted
+    val rowExpandStars = expandRows(stars, emptyRows, 2-1)
+
+    val starsOnColumn = rowExpandStars.groupBy(_._2)
+    val minColumn = rowExpandStars.map(_._2).min
+    val maxColumn = rowExpandStars.map(_._2).max
+    val emptyColumns = ((minColumn to maxColumn).toSet diff starsOnColumn.keySet).toList.sorted
+    val expandedGalaxy = expandColumns(rowExpandStars, emptyColumns, 2-1)
+    expandedGalaxy.map(a => expandedGalaxy.map(b => distance(a, b)).sum).sum / 2
+
+  override def exampleAnswerPart2: Long = 82000210
+  override def solvePart2(lines: List[String]): Long =
+    val stars = lines.zipWithIndex.flatMap(r => r._1.toCharArray.zipWithIndex.filter(_._1 == '#').map(s => (r._2.toLong, s._2.toLong)))
+
+    val starsOnRow = stars.groupBy(_._1)
+    val minRow = stars.map(_._1).min
+    val maxRow = stars.map(_._1).max
+    val emptyRows = ((minRow to maxRow).toSet diff starsOnRow.keySet).toList.sorted
+    val rowExpandStars = expandRows(stars, emptyRows, 1000000-1)
+
+    val starsOnColumn = rowExpandStars.groupBy(_._2)
+    val minColumn = rowExpandStars.map(_._2).min
+    val maxColumn = rowExpandStars.map(_._2).max
+    val emptyColumns = ((minColumn to maxColumn).toSet diff starsOnColumn.keySet).toList.sorted
+    val expandedGalaxy = expandColumns(rowExpandStars, emptyColumns, 1000000-1)
+    expandedGalaxy.map(a => expandedGalaxy.map(b => distance(a, b)).sum).sum / 2
+
+}
+
+object Day11 extends App {
+  new Day11().solvePuzzles()
+}
