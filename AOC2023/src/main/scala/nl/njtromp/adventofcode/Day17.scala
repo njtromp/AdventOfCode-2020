0efce2126e611bc03d3895efd573a71aa8d617c5
Nico Tromp <nico.tromp@gmail.com>
2024-10-05 09:43:21 +0200

    Solved part 2 of day 17.

--- AOC2023/src/main/scala/nl/njtromp/adventofcode/Day17.scala
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day17.scala
@@ -3,30 +3,31 @@ package nl.njtromp.adventofcode
 import scala.collection.mutable
 
 class Day17 extends Puzzle[Long] with SimpleMapTypes {
-  private val maxStraight = 3
 
-  private case class State(pos: Pos, dir: Delta, length: Int)
+  private case class State(position: Pos, direction: Delta, straight: Int)
 
-  private def pushCrucible(heatLoss: SimpleMap[Int], start: Pos, finish: Pos): Long =
+  private def pushCrucible(heatLoss: SimpleMap[Int], start: Pos, finish: Pos, minStraight: Int, maxStraight: Int): Long =
     val heatLosses = mutable.Map.empty[State, Int].withDefaultValue(Int.MaxValue)
     val toBeVisited = mutable.PriorityQueue.empty[State](Ordering.by(s => heatLosses(s))).reverse
-    def pushCrucible(): Long =
+    def dijkstra(): Long =
       while toBeVisited.nonEmpty do
         val current = toBeVisited.dequeue()
-        if current.pos == finish then
-          return heatLosses(current)
+        if current.position == finish then
+          if current.straight >= minStraight then
+            return heatLosses(current)
         else
-          val directions = square
+          // Continue in the same directions until the minimum length is reached
+          val directions = (if current.position != start && current.straight < minStraight then List(current.direction) else square)
             // Not going back
-            .filter(d => !(d._1 == -current.dir._1 && d._2 == -current.dir._2))
+            .filter(d => !(d._1 == -current.direction._1 && d._2 == -current.direction._2))
             // Can't continue in a straight line for too long
-            .filter(d => d != current.dir || current.length < maxStraight)
+            .filter(d => d != current.direction || current.straight < maxStraight)
             // Must stay within the city
-            .filter(d => heatLoss.isOnMap(heatLoss.move(current.pos, d)))
+            .filter(d => heatLoss.isOnMap(heatLoss.move(current.position, d)))
           directions.foreach(d =>
-            val newPos = heatLoss.move(current.pos, d)
-            val newState = if d == current.dir then
-              State(newPos, d, current.length + 1)
+            val newPos = heatLoss.move(current.position, d)
+            val newState = if d == current.direction then
+              State(newPos, d, current.straight + 1)
             else
               State(newPos, d, 1)
             if heatLosses(current) + heatLoss(newPos) < heatLosses(newState) then
@@ -39,17 +40,19 @@ class Day17 extends Puzzle[Long] with SimpleMapTypes {
     val starting = State(start, down, 0)
     heatLosses(starting) = 0
     toBeVisited.enqueue(starting)
-    pushCrucible()
+    dijkstra()
 
   override def exampleAnswerPart1: Long = 102
   override def solvePart1(lines: List[String]): Long =
     val heatLoss = SimpleMap[Int](lines, _.toCharArray.map(_.asDigit))
     // Start at upper left and finish at lower right corner
-    pushCrucible(heatLoss, (0, 0), (heatLoss.height - 1, heatLoss.width - 1))
+    pushCrucible(heatLoss, (0, 0), (heatLoss.height - 1, heatLoss.width - 1), 0, 3)
 
-  override def exampleAnswerPart2: Long = 0
+  override def exampleAnswerPart2: Long = 94
   override def solvePart2(lines: List[String]): Long =
-    -1
+    val heatLoss = SimpleMap[Int](lines, _.toCharArray.map(_.asDigit))
+    // Start at upper left and finish at lower right corner
+    pushCrucible(heatLoss, (0, 0), (heatLoss.height - 1, heatLoss.width - 1), 4, 10)
 
 }
 
