e59e52d77af0135c6eacf2ca0f14c80b06fc8a77
Nico Tromp <nico.tromp@gmail.com>
2024-10-10 10:22:58 +0200

    Part 2 of day 12 works, caching results was key.

--- AOC2023/src/main/scala/nl/njtromp/adventofcode/Day12.scala
+++ AOC2023/src/main/scala/nl/njtromp/adventofcode/Day12.scala
@@ -1,98 +1,107 @@
 package nl.njtromp.adventofcode
 
-import scala.util.matching.Regex
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+import scala.collection.parallel.CollectionConverters._
 
 class Day12 extends Puzzle[Long] {
-  private case class Record(springs: String, damageCounts: List[Int]) {
-    lazy val cleaned: String = shrink(springs)
+  private case class Record(springs: String, groups: List[Int]) {
+    lazy val reduced: String = reduceWorkingSprings(springs)
+    lazy val cleaned: Record = removeMatchingEndGroups(Record(reduced, this.groups))
+    // If a group at either sides matches exactly it can be removed
+    @tailrec
+    private def removeMatchingEndGroups(record: Record): Record =
+      def cleanHead(record: Record): Record =
+        if record.groups.isEmpty then
+          record
+        else
+          val headMatch = s"^(([#|\\?]{${record.groups.head}})(\\.))".r
+          val head = record.reduced.take(record.groups.head + 1)
+          val tail = record.reduced.drop(record.groups.head + 1)
+          if headMatch.matches(head) && head.contains("#") then {
+            Record(reduceWorkingSprings(tail), record.groups.tail)
+          }
+          else
+            record
+      def cleanTail(record: Record): Record =
+        if record.groups.isEmpty then
+          record
+        else
+          val tailMatch = s"((\\.)([#|\\?]{${record.groups.last}}))$$".r
+          val tail = record.reduced.takeRight(record.groups.last + 1)
+          val head = record.reduced.dropRight(record.groups.last + 1)
+          if tailMatch.matches(tail) && tail.contains("#") then
+            Record(reduceWorkingSprings(head), record.groups.dropRight(1))
+          else
+            record
+      val cleaned = cleanHead(cleanTail(record))
+      if cleaned == record then
+        record
+      else
+        removeMatchingEndGroups(cleaned)
   }
 
   private def parseLine(line: String): Record =
     val parts = line.split(' ')
-    Record(shrink(parts.head), parts.last.split(',').map(_.toInt).toList)
+    Record(parts.head, parts.last.split(',').map(_.toInt).toList)
 
-  private def shrink(springs: String): String =
+  // Reduces multiple working springs to just 1 and removes any working springs on the left and right side
+  private def reduceWorkingSprings(springs: String): String =
     springs.replaceAll("\\.+", "\\.").replaceAll("(^\\.+)|(\\.+$)", "")
 
-  private def findMatches(text: String, regexs: List[Regex]): List[Int] =
-    def findMatches(text: String, offset: Int, regex: Regex): Set[Int] =
-      regex.findFirstMatchIn(text) match {
-        case Some(m) =>
-          findMatches(text.drop(1), offset + 1, regex) + (m.start + offset + 1)
-        case None =>
-          Set.empty
-      }
-    val headMatch = regexs.head.findFirstMatchIn(text)
-    val midMatches = findMatches(text, 0, regexs(1)).toList.sorted
-    val tailMatch = regexs.last.findFirstMatchIn(text)
-    (headMatch, tailMatch) match {
-      case (None, None) =>
-        midMatches
-      case (Some(h), None) =>
-        h.start :: midMatches.filter(_ > h.start)
-      case (None, Some(t)) =>
-        midMatches.filter(_ < t.start + 1) ++ List(t.start + 1)
-      case (Some(h), Some(t)) =>
-        h.start :: midMatches.filter(m => m > h.start && m < t.start + 1) ++ List(t.start + 1)
-    }
-
-  private def countPermutations(record: Record): Long =
-    def findLava(text: String): Set[Int] = text.zipWithIndex.filter(_._1 == '#').map(_._2).toSet
-    def countPermutations(matches: List[List[Int]], lengths: List[Int], pattern: String): Long =
-      matches match {
-        case Nil =>
-          0
-        case ms :: Nil =>
-          if ms.nonEmpty then
-            val matchingPatterns = ms.map(m => pattern + "." * (m - pattern.length) + "#" * lengths.head)
-              .filter(p =>
-                val fixed = findLava(record.cleaned.substring(0, p.length))
-                fixed.isEmpty || (findLava(p) intersect fixed).size == fixed.size
-              )
-            matchingPatterns.length
-          else
+  private def countCombinations(record: Record): Long =
+    val cache = mutable.Map.empty[(String, List[Int]), Long]
+    def countCombinations(springs: String, groups: List[Int]): Long = {
+      val key = (springs, groups)
+      if cache.contains(key) then
+        cache(key)
+      else
+        val result: Long =
+          if groups.isEmpty then
+            if !springs.contains('#') then 1 else 0
+          else if springs.isEmpty then
             0
-        case ms :: tail =>
-          ms.map(m =>
-            val filtered = tail.map(_.filter(_ > m + lengths.head))
-            if filtered.nonEmpty then
-              val newPattern = pattern + "." * (m - pattern.length) + "#" * lengths.head
-              countPermutations(filtered, lengths.tail, newPattern)
-            else
-              0
-          ).sum
-      }
-    val matches = record.damageCounts.indices.map(i =>
-      val length = record.damageCounts(i)
-      val sep = "(\\.|\\?)"
-      val matcher = s"([#|\\?]{$length})"
-      val regexs = List(
-        s"^($matcher$sep)".r,
-        s"($sep$matcher$sep)".r,
-        s"($sep$matcher)$$".r
-      )
-      val matches = findMatches(record.cleaned, regexs)
-//      println(matches)
-      matches
-    ).toList
-//    println(record.cleaned)
-    countPermutations(matches, record.damageCounts, "")
+          else
+            val springSize = springs.length
+            val headGroupSize = groups.head
+            val tailSize = if groups.tail.isEmpty then 0 else groups.tail.sum + groups.tail.size - 1
+            (0 to springSize - headGroupSize - tailSize)
+              .filter(n =>
+                val headMatch = if n == springSize - headGroupSize - tailSize then
+                  s"^(([\\.\\?]{$n})([#\\?]{$headGroupSize}))$$".r
+                else if n == 0 then
+                  s"^([#\\?]{$headGroupSize})([\\.\\?])(.*)".r
+                else
+                  s"^(([\\.\\?]{$n})([#\\?]{$headGroupSize}))([\\.\\?])(.*)".r
+                headMatch.matches(springs)
+              )
+              .map(n =>
+                val extra = if n + headGroupSize + 1 <= springSize then 1 else 0
+                val tail = springs.drop(n + headGroupSize + extra)
+                // Remove any leading dots (= working spring)
+                countCombinations(tail.dropWhile(_ == '.'), groups.tail)
+              ).sum
+        cache(key) = result
+        result
+    }
+    countCombinations(record.cleaned.springs, record.cleaned.groups)
 
   private def unfold(record: Record): Record =
-    Record((record.springs + '?').repeat(5).dropRight(1), (1 to 5).toList.flatMap(_ => record.damageCounts))
+    val unfoldCount = 5
+    Record((record.springs + '?').repeat(unfoldCount).dropRight(1), (1 to unfoldCount).toList.flatMap(_ => record.groups))
 
   override def exampleAnswerPart1: Long = (1 + 4 + 1 + 1 + 4 + 10)
-  override def solvePart1(lines: List[String]): Long = {
-    val bla = lines.map(parseLine).map(countPermutations)
-//    println(bla)
-    bla.sum
-  }
+  override def solvePart1(lines: List[String]): Long =
+    lines.map(parseLine)
+      .map(countCombinations)
+      .sum
 
   override def exampleAnswerPart2: Long = 525152
   override def solvePart2(lines: List[String]): Long =
-//    println("\n\n")
-    lines.map(l => unfold(parseLine(l))).map(countPermutations).sum
-//    -1
+    lines.map(l => unfold(parseLine(l)))
+      .map(countCombinations)
+      .sum
 
 }
 
