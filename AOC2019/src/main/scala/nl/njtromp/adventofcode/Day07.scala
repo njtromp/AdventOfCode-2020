36c89156807e98899d0d72663f761b96a5b8c33f
Nico Tromp <nico.tromp@gmail.com>
2024-10-13 11:53:26 +0200

    Converted memory to Array[Long] for day 7.

--- AOC2019/src/main/scala/nl/njtromp/adventofcode/Day07.scala
+++ AOC2019/src/main/scala/nl/njtromp/adventofcode/Day07.scala
@@ -13,16 +13,15 @@ class Day07 extends Puzzle[Long] {
   private val LESS_THEN = 7
   private val EQUALS = 8
 
-  // TODO change program to array of Long
-  private def execute(program: Array[Int], phaseSetting: Int, inputValue: Int): Long =
+  private def execute(program: Array[Long], phaseSetting: Int, inputValue: Long): Long =
     var outputValue: Long = 0
-    def read(mode: Int, value: Int): Int =
-      if mode % 10 == IMMEDIATE then value else program(value)
-    def biFunction(ip: Int, mode: Int, f: (Int, Int) => Int): Int =
-      program(program(ip + 3)) = f(read(mode, program(ip + 1)), read(mode / 10, program(ip + 2)))
+    def read(mode: Int, value: Long): Long =
+      if mode % 10 == IMMEDIATE then value else program(value.toInt)
+    def biFunction(ip: Int, mode: Int, f: (Long, Long) => Long): Int =
+      program(program(ip + 3).toInt) = f(read(mode, program(ip + 1)), read(mode / 10, program(ip + 2)))
       ip + 4
     def input(ip: Int, input: Int): Int =
-      program(program(ip + 1)) = input
+      program(program(ip + 1).toInt) = input
       ip + 2
     def output(ip: Int, mode: Int): Int =
       if outputValue != 0 then
@@ -31,58 +30,58 @@ class Day07 extends Puzzle[Long] {
       ip + 2
     def jumpIfTrue(ip: Int, mode: Int): Int =
       if read(mode, program(ip + 1)) != 0 then
-        read(mode / 10, program(ip + 2))
+        read(mode / 10, program(ip + 2)).toInt
       else
         ip + 3
     def jumpIfFalse(ip: Int, mode: Int): Int =
       if read(mode, program(ip + 1)) == 0 then
-        read(mode / 10, program(ip + 2))
+        read(mode / 10, program(ip + 2)).toInt
       else
         ip + 3
     def lessThen(ip: Int, mode: Int): Int =
-      program(program(ip + 3)) =
+      program(program(ip + 3).toInt) =
         if read(mode, program(ip + 1)) < read(mode / 10, program(ip + 2)) then
           1
         else
           0
       ip + 4
     def equals(ip: Int, mode: Int): Int =
-      program(program(ip + 3)) = if read(mode, program(ip + 1)) == read(mode / 10, program(ip + 2)) then 1 else 0
+      program(program(ip + 3).toInt) = if read(mode, program(ip + 1)) == read(mode / 10, program(ip + 2)) then 1 else 0
       ip + 4
 
-    var ip = 0
-    var opcode = program(ip)
+    var ip: Int = 0
+    var opcode: Long = program(ip)
     var inputCount = -1
     while opcode != STOP do
       opcode % 100 match
         case ADD =>
-          ip = biFunction(ip, opcode / 100, (a, b) => a + b)
+          ip = biFunction(ip, opcode.toInt / 100, (a, b) => a + b)
         case MUL =>
-          ip = biFunction(ip, opcode / 100, (a, b) => a * b)
+          ip = biFunction(ip, opcode.toInt / 100, (a, b) => a * b)
         case INPUT =>
           inputCount += 1
-          ip = input(ip, if inputCount == 0 then phaseSetting else inputValue)
+          ip = input(ip, if inputCount == 0 then phaseSetting else inputValue.toInt)
         case OUTPUT =>
-          ip = output(ip, opcode / 100)
+          ip = output(ip, opcode.toInt / 100)
         case JUMP_IF_TRUE =>
-          ip = jumpIfTrue(ip, opcode / 100)
+          ip = jumpIfTrue(ip, opcode.toInt / 100)
         case JUMP_IF_FALSE =>
-          ip = jumpIfFalse(ip, opcode / 100)
+          ip = jumpIfFalse(ip, opcode.toInt / 100)
         case LESS_THEN =>
-          ip = lessThen(ip, opcode / 100)
+          ip = lessThen(ip, opcode.toInt / 100)
         case EQUALS =>
-          ip = equals(ip, opcode / 100)
+          ip = equals(ip, opcode.toInt / 100)
       opcode = program(ip)
     outputValue
 
-  private def runProgram(program: Array[Int], phaseSettings: IndexedSeq[Int]): Int =
-    var input = 0
+  private def runProgram(program: Array[Long], phaseSettings: IndexedSeq[Int]): Long =
+    var input: Long = 0
     phaseSettings.foreach(phaseSetting => input = execute(program.clone, phaseSetting, input).toInt)
     input
 
   def runPrograms(lines: List[String]): Long =
     lines.map(l => {
-      val program = l.split(",").map(_.toInt)
+      val program = l.split(",").map(_.toLong)
       (0 to 4).permutations.map(settings =>
         runProgram(program, settings)
       ).max
