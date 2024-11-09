beb7e040606178a86df5f8be70795ef8e8b29d59
Nico Tromp <nico.tromp@gmail.com>
2024-11-09 12:58:21 +0100

    Add comment about binary search to day 14.

--- AOC2019/src/main/scala/nl/njtromp/adventofcode/Day14.scala
+++ AOC2019/src/main/scala/nl/njtromp/adventofcode/Day14.scala
@@ -47,10 +47,12 @@ class Day14 extends Puzzle[Long] {
     val MAX_ORE = 1000000000000L 
     var fuel = 1L
     var ore = produceFuel(fuel, chemicals)
+    // Find amount of fuel above the limit
     while ore < MAX_ORE do
       fuel *= 2
       ore = produceFuel(fuel, chemicals)
     var low = fuel / 2
+    // Binary search
     while low + 1 != fuel && produceFuel(fuel, chemicals) > MAX_ORE do
       val mid = low + (fuel - low) / 2
       if produceFuel(mid, chemicals) < MAX_ORE then
