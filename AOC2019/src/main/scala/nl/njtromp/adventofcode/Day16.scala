625839155537f76e1f7fb3560b57161a1ceabd0f
Nico Tromp <nico.tromp@gmail.com>
2024-11-18 13:05:45 +0100

    Fix some mysterious type-o's

--- AOC2019/src/main/scala/nl/njtromp/adventofcode/Day16.scala
+++ AOC2019/src/main/scala/nl/njtromp/adventofcode/Day16.scala
@@ -28,14 +28,14 @@ class Day16 extends Puzzle[Long] {
         fft(repeats - 1, phase(digits))
 
     fft(repeats, digits)
+    
+  private def asNumber(digits: List[Int]): Long =
+    digits.foldLeft(0L)((a, d) => a * 10L + d)  
 
   override def exampleAnswerPart1: Long = 24176176L + 73745418L + 52432133L
   override def solvePart1(lines: List[String]): Long =
-    (if lines.length == 1 then lines else lines.take(3)).map(l => {
-      fft(100, l.toList.map(_.asDigit), Array(0, 1, 0, -1))
-        .take(8)
-        .foldLeft(0L)((a, d) => a * 10L + d)
-    }
+    (if lines.length == 1 then lines else lines.take(3)).map(l =>
+      asNumber(fft(100, l.toList.map(_.asDigit), Array(0, 1, 0, -1)).take(8))
     ).sum
 
   override def exampleAnswerPart2: Long = 84462026L + 78725270L + 53553731L
@@ -44,8 +44,7 @@ class Day16 extends Puzzle[Long] {
     (if lines.length == 1 then lines else lines.drop(3)).map(l =>
       val input = l.toList.map(_.asDigit)
       val offset = l.take(7).toInt
-      fft(100, (0 until 10000).toList.flatMap(_ => input), Array(0, 1, 0, -1)).slice(offset, offset + 8)
-        .foldLeft(0L)((a, d) => a * 10L + d)
+      asNumber(fft(100, (0 until 10000).toList.flatMap(_ => input), Array(0, 1, 0, -1)).slice(offset, offset + 8))
     ).sum
 
 }
