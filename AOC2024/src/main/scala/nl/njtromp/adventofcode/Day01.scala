c078a6b1ccc1a2700566f4bbd2893fbfcb9b8924
Nico Tromp <nico.tromp@gmail.com>
2024-12-01 06:14:20 +0100

    Solved part 1 and 2 of day 1.

--- AOC2024/src/main/scala/nl/njtromp/adventofcode/Day01.scala
+++ AOC2024/src/main/scala/nl/njtromp/adventofcode/Day01.scala
@@ -2,13 +2,19 @@ package nl.njtromp.adventofcode
 
 class Day01 extends Puzzle[Long] {
 
-  override def exampleAnswerPart1: Long = 0
+  override def exampleAnswerPart1: Long = 11
   override def solvePart1(lines: List[String]): Long =
-    -1
+    val pairs = lines.map(_.split(" ")).map(_.filter(_.nonEmpty))
+    val first = pairs.map(_.head.toLong).sorted
+    val last = pairs.map(_.last.toLong).sorted
+    first.zip(last).map((f, l) => Math.abs(f - l)).sum
 
-  override def exampleAnswerPart2: Long = 0
+  override def exampleAnswerPart2: Long = 31
   override def solvePart2(lines: List[String]): Long =
-    -1
+    val pairs = lines.map(_.split(" ")).map(_.filter(_.nonEmpty))
+    val first = pairs.map(_.head.toLong).sorted
+    val last: Map[Long, List[Long]] = pairs.map(_.last.toLong).groupBy(v => v).withDefaultValue(List.empty[Long])
+    first.map(f => f * last(f).size).sum
 
 }
 
