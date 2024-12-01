f6c9dac5aa6723b0f35a97ea920a3fef1f5a3966
Nico Tromp <nico.tromp@gmail.com>
2024-12-01 09:30:05 +0100

    Use proper regex to split the lists.

--- AOC2024/src/main/scala/nl/njtromp/adventofcode/Day01.scala
+++ AOC2024/src/main/scala/nl/njtromp/adventofcode/Day01.scala
@@ -4,14 +4,14 @@ class Day01 extends Puzzle[Long] {
 
   override def exampleAnswerPart1: Long = 11
   override def solvePart1(lines: List[String]): Long =
-    val pairs = lines.map(_.split(" ")).map(_.filter(_.nonEmpty))
+    val pairs = lines.map(_.split(" {3}"))
     val first = pairs.map(_.head.toLong).sorted
     val last = pairs.map(_.last.toLong).sorted
     first.zip(last).map((f, l) => Math.abs(f - l)).sum
 
   override def exampleAnswerPart2: Long = 31
   override def solvePart2(lines: List[String]): Long =
-    val pairs = lines.map(_.split(" ")).map(_.filter(_.nonEmpty))
+    val pairs = lines.map(_.split(" {3}"))
     val first = pairs.map(_.head.toLong).sorted
     val last: Map[Long, List[Long]] = pairs.map(_.last.toLong).groupBy(v => v).withDefaultValue(List.empty[Long])
     first.map(f => f * last(f).size).sum
