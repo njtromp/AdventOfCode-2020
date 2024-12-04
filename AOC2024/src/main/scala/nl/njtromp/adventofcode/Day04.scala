2e38690fc95f0bc236e03095bf9b38606e899e05
Nico Tromp <nico.tromp@gmail.com>
2024-12-04 21:16:18 +0100

    Replace (x)mas variable names with word, just to keep is generic ;-)

--- AOC2024/src/main/scala/nl/njtromp/adventofcode/Day04.scala
+++ AOC2024/src/main/scala/nl/njtromp/adventofcode/Day04.scala
@@ -2,18 +2,18 @@ package nl.njtromp.adventofcode
 
 class Day04 extends Puzzle[Long] with SimpleMapTypes {
 
-  private def countWord(xmas: String, map: SimpleMap[Char]): Long =
-    map.find(xmas.head)
-      .flatMap(x => ALL_DIRECTIONS.map(d => map.getElements(x, d, xmas.length).mkString))
-      .count(_ == xmas)
+  private def countWord(word: String, map: SimpleMap[Char]): Long =
+    map.find(word.head)
+      .flatMap(x => ALL_DIRECTIONS.map(d => map.getElements(x, d, word.length).mkString))
+      .count(_ == word)
 
-  private def countXWord(mas: String, map: SimpleMap[Char]): Long = {
-    val halfLength = mas.length / 2
-    map.find(_ == mas(halfLength))
+  private def countXWord(word: String, map: SimpleMap[Char]): Long = {
+    val halfLength = word.length / 2
+    map.find(_ == word(halfLength))
       .count(x =>
         DIAGONAL.filter(d => map.isOnMap(map.moveOpposite(x, d, halfLength)))
-          .map(d => map.getElements(map.moveOpposite(x, d, halfLength), d, mas.length).mkString)
-          .count(_ == mas) == 2
+          .map(d => map.getElements(map.moveOpposite(x, d, halfLength), d, word.length).mkString)
+          .count(_ == word) == 2
       )
   }
 
