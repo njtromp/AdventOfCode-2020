9d114466bdffdd0294a8845f0cac4d0847ff5f55
Nico Tromp <nico.tromp@gmail.com>
2024-12-04 18:22:21 +0100

    Move word extracting from puzzle to SimpleMap, might come in handy.

--- AOC2024/src/main/scala/nl/njtromp/adventofcode/Day04.scala
+++ AOC2024/src/main/scala/nl/njtromp/adventofcode/Day04.scala
@@ -2,39 +2,30 @@ package nl.njtromp.adventofcode
 
 class Day04 extends Puzzle[Long] with SimpleMapTypes {
 
-  private def findWord(xmas: String, map: SimpleMap[Char]): Long =
-    def createWord(x: Pos, d: Delta, length: Int): String =
-      if length == 0 || !map.isOnMap(x) then
-        ""
-      else
-        val m = map.move(x, d)
-        map(x) + createWord(m, d, length - 1)
-    map.find(xmas.head).flatMap(x =>
-      all.map(d => createWord(x, d, xmas.length)))
-      .count(_ == xmas
-    )
+  private def countWord(xmas: String, map: SimpleMap[Char]): Long =
+    map.find(xmas.head)
+      .flatMap(x => ALL_DIRECTIONS.map(d => map.getElements(x, d, xmas.length).mkString))
+      .count(_ == xmas)
 
-  private def findX_MAS(map: SimpleMap[Char]): Long =
-    def createMAS(a: Pos, d: Delta): String =
-      val m = map.move(a, d)
-      val s = map.moveOpposite(a, d)
-      if map.isOnMap(m) && map.isOnMap(s) then
-        s"${map(m)}A${map(s)}"
-      else
-        ""
-    def isX_MAS(a: (Int, Int), deltas: List[(Int, Int)]): Boolean =
-      deltas.count(d => List("MAS", "SAM").contains(createMAS(a, d))) == 2
-    map.find(_ == 'A').count(isX_MAS(_, List(upRight, downRight)))
+  private def countXWord(mas: String, map: SimpleMap[Char]): Long = {
+    val halfLength = mas.length / 2
+    map.find(_ == mas(halfLength))
+      .count(x =>
+        DIAGONAL.filter(d => map.isOnMap(map.moveOpposite(x, d, halfLength)))
+          .map(d => map.getElements(map.moveOpposite(x, d, halfLength), d, mas.length).mkString)
+          .count(_ == mas) == 2
+      )
+  }
 
   override def exampleAnswerPart1: Long = 18
   override def solvePart1(lines: List[String]): Long =
     val map = SimpleMap[Char](lines, _.toCharArray)
-    findWord("XMAS", map)
+    countWord("XMAS", map)
 
   override def exampleAnswerPart2: Long = 9
   override def solvePart2(lines: List[String]): Long =
     val map = SimpleMap[Char](lines, _.toCharArray)
-    findX_MAS(map)
+    countXWord("MAS", map)
 
 }
 
