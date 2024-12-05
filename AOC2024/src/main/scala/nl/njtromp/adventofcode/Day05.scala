0555d14fc26c3ff834bfd95d6f8af3bdb5a05ddd
Nico Tromp <nico.tromp@gmail.com>
2024-12-05 08:11:58 +0100

    Solved part 1 and 2 of day 5.

---
+++ AOC2024/src/main/scala/nl/njtromp/adventofcode/Day05.scala
@@ -0,0 +1,55 @@
+package nl.njtromp.adventofcode
+
+import scala.collection.mutable
+
+class Day05 extends Puzzle[Long] {
+
+  private def parseRules(rules: List[String]): List[(Long, Long)] =
+    rules.map(rule =>
+      val r = rule.split('|')
+      r.head.toLong -> r.last.toLong
+    )
+
+  private def parseUpdates(updates: List[String]): List[List[Long]] =
+    updates.map(_.split(',').map(_.toLong).toList)
+
+  private def isValid(updates: List[Long], rules: List[(Long, Long)]): Boolean =
+    val applicableRules = rules.filter(r => updates.contains(r._1) && updates.contains(r._2))
+    applicableRules.forall(r => updates.indexOf(r._1) < updates.indexOf(r._2))
+
+  private def correctOrder(updates: List[Long], rules: List[(Long, Long)]): List[Long] =
+    val applicableRules = rules.filter(r => updates.contains(r._1) && updates.contains(r._2)).map(r => (r._1.toInt, r._2.toInt))
+    val indexes = Array.ofDim[Int](updates.max.toInt + 1)
+    updates.zipWithIndex.foreach(u => indexes(u._1.toInt) = u._2)
+    def isValid: Boolean =
+      applicableRules.forall(r => indexes(r._1) < indexes(r._2))
+    while !isValid do
+      applicableRules.filter(r => indexes(r._1) >= indexes(r._2))
+        .foreach(r => indexes(r._2) += 1)
+    updates.map(u => (u, indexes(u.toInt)))
+      .sortBy(_._2)
+      .map(_._1)
+
+
+  override def exampleAnswerPart1: Long = 143
+  override def solvePart1(lines: List[String]): Long =
+    val grouped = groupByEmptyLine(lines)
+    val rules = parseRules(grouped.head)
+    val updates = parseUpdates(grouped.last)
+    updates.filter(isValid(_, rules))
+      .map(u => u(u.size / 2)).sum
+
+  override def exampleAnswerPart2: Long = 123
+  override def solvePart2(lines: List[String]): Long =
+    val grouped = groupByEmptyLine(lines)
+    val rules = parseRules(grouped.head)
+    val updates = parseUpdates(grouped.last)
+    updates.filterNot(isValid(_, rules))
+      .map(correctOrder(_, rules))
+      .map(u => u(u.size / 2)).sum
+
+}
+
+object Day05 extends App {
+  new Day05().solvePuzzles()
+}
