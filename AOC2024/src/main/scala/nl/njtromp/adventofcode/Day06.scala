8cd8878282eaee192f8e735cb4081ffa964e757d
Nico Tromp <nico.tromp@gmail.com>
2024-12-06 17:10:09 +0100

    Remove unnecessary variables and use more functional style(ish) methods.

--- AOC2024/src/main/scala/nl/njtromp/adventofcode/Day06.scala
+++ AOC2024/src/main/scala/nl/njtromp/adventofcode/Day06.scala
@@ -5,16 +5,17 @@ import scala.collection.mutable
 
 class Day06 extends Puzzle[Long] with SimpleMapTypes {
 
-  private def turnRight(d: Delta): Delta =
-    d match
+  private def turnRight(dir: Delta): Delta =
+    dir match
       case UP => RIGHT
       case RIGHT => DOWN
       case DOWN => LEFT
       case LEFT => UP
 
-  private def followGuard(guard: Pos, dir: Delta, map: SimpleMap[Char], path: mutable.Set[Pos]): mutable.Set[Pos] =
+  private def followGuard(guard: Pos, dir: Delta, map: SimpleMap[Char]): mutable.Set[Pos] =
+    val path = mutable.Set(guard)
     @tailrec
-    def follow(guard: Pos, dir: Delta): Unit =
+    def follow(guard: Pos, dir: Delta): mutable.Set[Pos] =
       val moved = map.move(guard, dir)
       if map.isOnMap(moved) then
         if map(moved) != '#' then
@@ -22,12 +23,12 @@ class Day06 extends Puzzle[Long] with SimpleMapTypes {
           follow(moved, dir)
         else
           follow(guard, turnRight(dir))
-    path += guard
+      else
+        path
     follow(guard, dir)
-    path
 
-  private def makesLoop(guard: Pos, dir: Delta, map: SimpleMap[Char]): Boolean =
-    val path = mutable.Set.empty[(Pos, Delta)]
+  private def containsLoop(guard: Pos, dir: Delta, map: SimpleMap[Char]): Boolean =
+    val path = mutable.Set((guard, UP))
     @tailrec
     def follow(guard: Pos, dir: Delta): Boolean =
       val moved = map.move(guard, dir)
@@ -40,26 +41,25 @@ class Day06 extends Puzzle[Long] with SimpleMapTypes {
         follow(moved, dir)
       else
         follow(guard, turnRight(dir))
-    path += ((guard, UP))
     follow(guard, dir)
 
   override def exampleAnswerPart1: Long = 41
   override def solvePart1(lines: List[String]): Long =
     val map = SimpleMap(lines, _.toArray)
     val guard = map.find('^').head
-    followGuard(guard, UP, map, mutable.Set.empty).size
+    followGuard(guard, UP, map).size
 
   override def exampleAnswerPart2: Long = 6
   override def solvePart2(lines: List[String]): Long =
     val map = SimpleMap(lines, _.toArray)
     val guard = map.find('^').head
-    val spaces = map.find('.')
-    spaces.count(c =>
-      map(c) = '#'
-      val isLoop = makesLoop(guard, UP, map)
-      map(c) = '.'
-      isLoop
-    )
+    map.find('.')
+      .count(dot =>
+        map(dot) = '#'
+        val hasLoop = containsLoop(guard, UP, map)
+        map(dot) = '.'
+        hasLoop
+      )
 
 }
 
