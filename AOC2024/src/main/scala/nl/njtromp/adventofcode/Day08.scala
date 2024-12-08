34c99932c93ed6e0492b554ecc0516f38e6439fb
Nico Tromp <nico.tromp@gmail.com>
2024-12-08 16:32:49 +0100

    Finally generalized marking the anti-nodes.

--- AOC2024/src/main/scala/nl/njtromp/adventofcode/Day08.scala
+++ AOC2024/src/main/scala/nl/njtromp/adventofcode/Day08.scala
@@ -2,30 +2,13 @@ package nl.njtromp.adventofcode
 
 class Day08 extends Puzzle[Long] with SimpleMapTypes {
 
-  private def markAntiNodes1(map: SimpleMap[Char]): Unit =
-    def markAntiNodes(antennas: (Char, List[Pos])): Unit =
+  private def markAntiNodes(map: SimpleMap[Char], min: Int, max: Int): Set[Pos] =
+    def markAntiNodes(antennas: (Char, List[Pos])): Set[Pos] =
       val as = antennas._2.toSet
-      val antiNodes = as.flatMap(a =>
+      as.flatMap(a =>
         as.filterNot(_ == a).flatMap(b =>
           val d = a - b
-          Set(
-            map.move(b, d),
-            map.moveOpposite(a, d),
-          )
-            .filter(map.isOnMap)
-        )
-      )
-      antiNodes.foreach(map(_) = '#')
-    val antennas = map.find(_ != '.').groupBy(map(_))
-    antennas.foreach(markAntiNodes)
-
-  private def markAntiNodes2(map: SimpleMap[Char]): Unit =
-    def markAntiNodes(antennas: (Char, List[Pos])): Unit =
-      val as = antennas._2.toSet
-      val antiNodes = as.flatMap(a =>
-        as.filterNot(_ == a).flatMap(b =>
-          val d = a - b
-          (1 to map.height).indices.flatMap(l =>
+          (min to max).flatMap(l =>
             Set(
               map.move(b, d, l),
               map.moveOpposite(a, d, l),
@@ -34,21 +17,18 @@ class Day08 extends Puzzle[Long] with SimpleMapTypes {
           )
         )
       )
-      antiNodes.foreach(map(_) = '#')
-    val antennas = map.find(_ != '.').groupBy(map(_))
-    antennas.foreach(markAntiNodes)
+    val antennaGroups = map.find(_ != '.').groupBy(map(_))
+    antennaGroups.toSet.flatMap(markAntiNodes)
 
   override def exampleAnswerPart1: Long = 14
   override def solvePart1(lines: List[String]): Long =
     val map = SimpleMap(lines)
-    markAntiNodes1(map)
-    map.find('#').size
+    markAntiNodes(map, 1, 1).size
 
   override def exampleAnswerPart2: Long = 34
   override def solvePart2(lines: List[String]): Long =
     val map = SimpleMap(lines)
-    markAntiNodes2(map)
-    map.find('#').size
+    markAntiNodes(map, 0, map.height).size
 
 }
 
