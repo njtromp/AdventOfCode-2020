c7eb0e649e036885960f9c73ae39add56d1b3339
Nico Tromp <nico.tromp@gmail.com>
2024-12-10 09:35:16 +0100

    Refactor to reduce #-of lines.

--- AOC2024/src/main/scala/nl/njtromp/adventofcode/Day10.scala
+++ AOC2024/src/main/scala/nl/njtromp/adventofcode/Day10.scala
@@ -3,19 +3,13 @@ package nl.njtromp.adventofcode
 class Day10 extends Puzzle[Long] with SimpleMapTypes {
 
   private def findTrails(starts: List[Pos], map: SimpleMap[Int], unique: Boolean): List[Pos] =
+    def partSelector(trail: List[Pos]): List[Pos] = if unique then trail.toSet.toList else trail
     def findTrail(current: Pos): List[Pos] =
       if map(current) == 9 then
         List(current)
       else
-        val trail = map.neighborPositions(current, SQUARE).filter(map(_) == map(current) + 1)
-        trail.flatMap(findTrail)
-    starts.flatMap(s =>
-      val trail = findTrail(s)
-      if unique then
-        trail.toSet
-      else
-        trail
-    )
+        map.neighborPositions(current, SQUARE).filter(map(_) == map(current) + 1).flatMap(findTrail)
+    starts.flatMap(s => partSelector(findTrail(s)))
 
   override def exampleAnswerPart1: Long = 36
   override def solvePart1(lines: List[String]): Long =
