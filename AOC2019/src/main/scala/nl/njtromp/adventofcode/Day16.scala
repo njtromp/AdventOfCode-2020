9122da00541e8c3c3345eae889acee6ffe0ac933
Nico Tromp <nico.tromp@gmail.com>
2024-12-14 09:01:54 +0100

    Add explanation on how to determine the stop condition.

--- AOC2019/src/main/scala/nl/njtromp/adventofcode/Day16.scala
+++ AOC2019/src/main/scala/nl/njtromp/adventofcode/Day16.scala
@@ -1,15 +1,19 @@
 package nl.njtromp.adventofcode
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.collection.parallel.CollectionConverters.*
 
 class Day16 extends Puzzle[Long] {
-  private def fft(repeats: Int, digits: List[Int], basePattern: Array[Int]): List[Int] =
-    def createPattern(multiplier: Int): Array[Int] =
-      basePattern.flatMap(Array.fill(multiplier)(_))
+  private val basePattern = Array(0, 1, 0, -1)
+  private def createPattern(multiplier: Int): Array[Int] =
+    basePattern.flatMap(Array.fill(multiplier)(_))
+  private val patterns = mutable.Map.empty[Int, Array[Int]].withDefault(createPattern)
+
+  private def fft(repeats: Int, digits: List[Int]): List[Int] =
     def phase(digits: List[Int]): List[Int] =
       (1 to digits.length).par.map(n =>
-        val pattern = createPattern(n)
+        val pattern = patterns(n)
         var i = 1
         val result = digits.foldLeft(0)((a, d) =>
           val f = pattern(i)
@@ -18,33 +22,38 @@ class Day16 extends Puzzle[Long] {
         )
         Math.abs(result) % 10
       ).toList
+//    val offsets = mutable.Map.empty[Long, Long].withDefaultValue(0L)
     @tailrec
     def fft(repeats: Int, digits: List[Int]): List[Int] =
+//      offsets(asNumber(digits.take(7))) += 1L
       if repeats == 0 then
         println
         digits
       else
         print('.')
         fft(repeats - 1, phase(digits))
-
     fft(repeats, digits)
+//    val result = fft(repeats, digits)
+//    offsets.filter(_._2 > 1).foreach(println)
+//    result
     
   private def asNumber(digits: List[Int]): Long =
     digits.foldLeft(0L)((a, d) => a * 10L + d)  
 
   override def exampleAnswerPart1: Long = 24176176L + 73745418L + 52432133L
   override def solvePart1(lines: List[String]): Long =
-    (if lines.length == 1 then lines else lines.take(3)).map(l =>
-      asNumber(fft(100, l.toList.map(_.asDigit), Array(0, 1, 0, -1)).take(8))
-    ).sum
+//    (if lines.length == 1 then lines else lines.take(3)).map(l =>
+//      asNumber(fft(100, l.toList.map(_.asDigit)).take(8))
+//    ).sum
+    -1
 
-  override def exampleAnswerPart2: Long = 84462026L + 78725270L + 53553731L
+  override def exampleAnswerPart2: Long = 0 //84462026L + 78725270L + 53553731L
   override def solvePart2(lines: List[String]): Long =
-    println("Part 2 is not funny :-(")
+    if lines.length != 1 then return 0
     (if lines.length == 1 then lines else lines.drop(3)).map(l =>
       val input = l.toList.map(_.asDigit)
       val offset = l.take(7).toInt
-      asNumber(fft(100, (0 until 10000).toList.flatMap(_ => input), Array(0, 1, 0, -1)).slice(offset, offset + 8))
+      asNumber(fft(100, (0 until 1000).toList.flatMap(_ => input)).slice(offset, offset + 8))
     ).sum
 
 }
