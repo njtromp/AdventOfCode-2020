af7887b2cb3439991d1fdd27dda38b42aded2f1c
Nico Tromp <nico.tromp@gmail.com>
2025-12-01 08:58:24 +0100

    Solved both puzzles of Day 1.

--- AOC2025/build.sbt
+++ AOC2025/build.sbt
@@ -1,9 +1,9 @@
 ThisBuild / version := "0.25.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "3.7.3"
+ThisBuild / scalaVersion := "3.7.4"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.4.0"
-libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "1.2.0"
+//libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "1.2.0"
 libraryDependencies += "org.apache.commons" % "commons-math3" % "3.6.1"
 
 lazy val root = (project in file("."))
--- AOC2025/src/main/scala/nl/njtromp/adventofcode/Day01.scala
+++ AOC2025/src/main/scala/nl/njtromp/adventofcode/Day01.scala
@@ -2,13 +2,40 @@ package nl.njtromp.adventofcode
 
 class Day01 extends Puzzle[Long] {
 
-  override def exampleAnswerPart1: Long = 0
-  override def solvePart1(lines: List[String]): Long =
-    -1
+  override def exampleAnswerPart1: Long = 3
+  override def solvePart1(lines: List[String]): Long = {
+    lines.foldLeft((50L, 0L))((a, l) =>
+      val rotation = (if l.charAt(0) == 'L' then -1L else 1L) * l.substring(1).toLong
+      val dial = (a._1 + rotation + 100L) % 100L
+      (dial, a._2 + (if dial == 0 then 1 else 0))
+    )._2
+  }
 
-  override def exampleAnswerPart2: Long = 0
-  override def solvePart2(lines: List[String]): Long =
-    -1
+  override def exampleAnswerPart2: Long = 6
+  override def solvePart2(lines: List[String]): Long = {
+    println("6183 is too high")
+    def rotate(dial: Long, rotation: Long): Long =
+      if rotation > 0 then {
+        val newDial = (dial + 101L) % 100L
+        if newDial == 0 then
+          1 + rotate(newDial, rotation - 1)
+        else
+          rotate(newDial, rotation - 1)
+      } else if rotation < 0 then
+        val newDial = (dial + 99L) % 100L
+        if newDial == 0 then
+          1 + rotate(newDial, rotation + 1)
+        else
+          rotate(newDial, rotation + 1)
+      else
+        0
+
+    lines.foldLeft((50L, 0L))((a, l) =>
+      val rotation = (if l.charAt(0) == 'L' then -1L else 1L) * l.substring(1).toLong
+      val dial = (a._1 + rotation + 100L) % 100L
+      (dial, a._2 + rotate(a._1, rotation))
+    )._2
+  }
 
 }
 
