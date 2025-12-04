d896deddaf921401fe34bbe04ae6630ed62464ea
Nico Tromp <nico.tromp@gmail.com>
2025-12-04 08:26:21 +0100

    Solved both puzzles of Day 4.

---
+++ AOC2025/src/main/scala/nl/njtromp/adventofcode/Day04.scala
@@ -0,0 +1,31 @@
+package nl.njtromp.adventofcode
+
+class Day04 extends Puzzle[Long] with SimpleMapTypes {
+  private val PAPER = '@'
+
+  override def exampleAnswerPart1: Long = 13
+  override def solvePart1(lines: List[String]): Long =
+    val map = SimpleMap(lines)
+    map.allPositions()
+      .filter(map(_) == PAPER)
+      .count(p => map.neighbors(p, ALL_DIRECTIONS).count(_ == PAPER) < 4)
+
+
+  override def exampleAnswerPart2: Long = 43
+  override def solvePart2(lines: List[String]): Long =
+    def removePaperRolls(map: SimpleMap[Char]): Long =
+      val removablePaper = map.allPositions()
+        .filter(map(_) == PAPER)
+        .filter(p => map.neighbors(p, ALL_DIRECTIONS).count(_ == PAPER) < 4)
+      if removablePaper.isEmpty then
+        0
+      else
+        removablePaper.foreach(map(_) = '.')
+        removablePaper.size + removePaperRolls(map)
+    removePaperRolls(SimpleMap(lines))
+
+}
+
+object Day04 extends App {
+  new Day04().solvePuzzles()
+}
