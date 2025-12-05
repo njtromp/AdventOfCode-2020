fa26f6ca1794701f260fe13ca31f453e21a12a14
Nico Tromp <nico.tromp@gmail.com>
2025-12-05 08:08:18 +0100

    Switch to using existing combine method, now day 5 is nearly just 2 oneliners ;-).

--- AOC2025/src/main/scala/nl/njtromp/adventofcode/Day05.scala
+++ AOC2025/src/main/scala/nl/njtromp/adventofcode/Day05.scala
@@ -12,22 +12,10 @@ class Day05 extends Puzzle[Long] {
 
   override def exampleAnswerPart2: Long = 14
   override def solvePart2(lines: List[String]): Long =
-    def merge(ranges: List[LongRange]): List[LongRange] =
-      if ranges.isEmpty then
-        Nil
-      else
-        val range = ranges.head
-        val overlapping = ranges.tail.filter(_.isOverlapping(range))
-        if overlapping.isEmpty then
-          range :: merge(ranges.tail)
-        else
-          val combined = overlapping.flatMap(_.combine(range))
-          merge(combined ++ ranges.tail.filterNot(overlapping.contains))
-
     val freshIds: List[LongRange] = lines.takeWhile(_.nonEmpty).map {
       case s"$first-$last" => LongRange(first.toLong, last.toLong)
     }
-    merge(freshIds).map(_.size).sum
+    LongRange.combine(freshIds).map(_.size).sum
 
 }
 
