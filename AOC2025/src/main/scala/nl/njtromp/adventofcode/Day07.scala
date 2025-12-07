f2fd7b45c301841300da67cb4f5a06d69f4b2b64
Nico Tromp <nico.tromp@gmail.com>
2025-12-07 17:36:42 +0100

    Solved both puzzles of Day 7.
    Cleaning the house and F1 had priority over solving part 2. Memoization to the rescue ;-)

---
+++ AOC2025/src/main/scala/nl/njtromp/adventofcode/Day07.scala
@@ -0,0 +1,55 @@
+package nl.njtromp.adventofcode
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+class Day07 extends Puzzle[Long] with SimpleMapTypes {
+  private val START = 'S'
+  private val SPLITTER = '^'
+
+  private def countSplits(map: SimpleMap[Char], beams: List[Pos]): Long =
+    @tailrec
+    def followBeams(beams: Set[Pos], nrOfSplits: Long): Long =
+      val oneDown = beams.map(_ + DOWN)
+      if map.isOnMap(oneDown.head) then
+        val splitters = oneDown.filter(map(_) == SPLITTER)
+        val straightDown = oneDown -- splitters
+        val splittedBeams = splitters.flatMap(s => Set(s + LEFT, s + RIGHT))
+        val newBeams = straightDown ++ splittedBeams
+        followBeams(newBeams, nrOfSplits + splitters.size)
+      else
+        nrOfSplits
+    followBeams(beams.toSet, 0L)
+
+  private val nrOfPaths = mutable.Map.empty[Pos, Long]
+  private def countParallelSplits(map: SimpleMap[Char], particle: Pos): Long =
+    def followParticle(particle: Pos): Long =
+      if !map.isOnMap(particle) then
+        1
+      else if nrOfPaths.contains(particle) then
+        nrOfPaths(particle)
+      else if map(particle) == SPLITTER then
+        val paths = followParticle(particle + LEFT) + followParticle(particle + RIGHT)
+        nrOfPaths(particle) = paths
+        paths
+      else
+        followParticle(particle + DOWN)
+    followParticle(particle)
+
+  override def exampleAnswerPart1: Long = 21
+  override def solvePart1(lines: List[String]): Long =
+    val map = SimpleMap(lines)
+    val beams = map.find(START)
+    countSplits(map, beams)
+
+  override def exampleAnswerPart2: Long = 40
+  override def solvePart2(lines: List[String]): Long =
+    val map = SimpleMap(lines)
+    val start = map.find(START).head
+    countParallelSplits(map, start)
+
+}
+
+object Day07 extends App {
+  new Day07().solvePuzzles()
+}
