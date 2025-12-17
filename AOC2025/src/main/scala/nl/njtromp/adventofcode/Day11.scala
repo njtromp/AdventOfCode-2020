b1385c9a873b99b5cfe2252b984f99f5d8d54e5a
Nico Tromp <nico.tromp@gmail.com>
2025-12-17 17:43:48 +0100

    Solve part 2 of day 11.

--- AOC2025/src/main/scala/nl/njtromp/adventofcode/Day11.scala
+++ AOC2025/src/main/scala/nl/njtromp/adventofcode/Day11.scala
@@ -2,7 +2,7 @@ package nl.njtromp.adventofcode
 
 import scala.collection.mutable
 
-class Day11 extends Puzzle[Long] {
+class Day11 extends Puzzle[Long] with RouteFinding {
   private val YOU = "you"
   private val OUT = "out"
   private val SVR = "svr"
@@ -11,26 +11,30 @@ class Day11 extends Puzzle[Long] {
 
   private case class Device(name: String, outputs: List[String])
 
+  private def findPaths(start: String, finish: String, devices: List[Device]): Long =
+    val deviceMap = devices.map(d => (d.name, d)).toMap
+    val nrOfPaths = mutable.Map.empty[String, Long]
+    def findPaths(current: String): Long =
+      if current == finish then
+        1
+      else if nrOfPaths.contains(current) then
+        nrOfPaths(current)
+      else
+        val paths = deviceMap(current).outputs.map(d => findPaths(d)).sum
+        nrOfPaths(current) = paths
+        paths
+    findPaths(start)
+
   override def exampleAnswerPart1: Long = 5
   override def solvePart1(lines: List[String]): Long =
     val devices = lines.splitOnEmptyLines.head.map(l => Device(l.substring(0, 3), l.split(' ').tail.toList))
-    val deviceMap = devices.map(d => (d.name, d)).toMap
-    val connections = mutable.Map[String, Long]().empty.withDefaultValue(0L)
-    connections(YOU) = 1
-    val needsVisiting = mutable.Queue[String]().empty
-    needsVisiting.enqueue(YOU)
-    while needsVisiting.nonEmpty do
-      val device: String = needsVisiting.dequeue()
-      if device != OUT then
-        deviceMap(device).outputs.foreach(o =>
-          connections(o) += 1
-          needsVisiting.enqueue(o)
-      )
-    connections(OUT)
+    findPaths(YOU, OUT, devices)
 
   override def exampleAnswerPart2: Long = 2
   override def solvePart2(lines: List[String]): Long =
-    -1
+    val devices = Device(OUT, List.empty) :: lines.splitOnEmptyLines.last.map(l => Device(l.substring(0, 3), l.split(' ').tail.toList))
+    // There is no path from DAC to FFT, so the path can be split into three sections :-)
+    findPaths(SVR, FFT, devices) * findPaths(FFT, DAC, devices) * findPaths(DAC, OUT, devices)
 
 }
 
